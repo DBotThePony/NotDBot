9945fffffdf8215314ffb892ce2c894e04305bc0
DBot <roboderpy@gmail.com>
2016-12-07 06:09:54 +0700

    Renamed modules -> lib; addons -> modules

--- app/init.js
+++ app/init.js
@@ -6,26 +6,26 @@ DBot.URLRootBare = 'dbot.serealia.ca/bot';
 DBot.URLRoot = 'https://' + DBot.URLRootBare;
 DBot.DBot = '141004095145115648';
 
-require('./modules/util.js');
+require('./lib/util.js');
 
-require('./modules/hook.js');
-require('./modules/mysql.js');
-require('./modules/cvars.js');
+require('./lib/hook.js');
+require('./lib/mysql.js');
+require('./lib/cvars.js');
 require('./generic.js');
-require('./modules/tags.js');
-require('./modules/commban.js');
+require('./lib/tags.js');
+require('./lib/commban.js');
 
 require('./handler.js');
 require('./commands.js');
 
-require('./modules/confirm.js');
+require('./lib/confirm.js');
 
-DBot.fs.readdirSync('./app/addons/').forEach(function(file) {
+DBot.fs.readdirSync('./app/modules/').forEach(function(file) {
 	var sp = file.split('.');
 	if (!sp[1] || sp[1] != 'js')
 		return;
 	
-	require('./addons/' + file);
+	require('./modules/' + file);
 });
 
 DBot.START_STAMP = (new Date()).getTime() / 1000;
---
+++ app/modules/rlogs.js
@@ -0,0 +1,5 @@
+
+const moment = require('moment');
+const utf8 = require('utf8');
+const hDuration = require('humanize-duration');
+
