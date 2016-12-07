834384e7f0964c4a9fcf678e621662579491f52d
DBot <roboderpy@gmail.com>
2016-12-07 16:03:29 +0700

    mcvar command

--- app/commands/svar.js
+++ app/commands/svar.js
@@ -166,6 +166,69 @@ DBot.RegisterCommand({
 });
 
 DBot.RegisterCommand({
+	name: 'mcvar',
+	
+	help_args: '<channel> <cvar> [new value]',
+	desc: 'Channel Variable manipulation.\nInstead of manipulation of current channel variables, manipulates\nspecified channel variables\nIf no value specified, prints it\'s value',
+	
+	func: function(args, cmd, msg) {
+		if (DBot.IsPM(msg))
+			return 'Onoh! It is PM ;n;';
+		
+		if (!args[0]) {
+			return DBot.CommandError('Invalid channel', 'mcvar', args, 1);
+		}
+		
+		if (!args[0].match(/^<#([0-9]+)>$/))
+			return DBot.CommandError('Invalid channel', 'mcvar', args, 1);
+		
+		let channelID = args[0].substr(2, args[0].length - 3);
+		let channel = DBot.FindChannel(channelID);
+		
+		if (!channel)
+			return DBot.CommandError('Invalid channel', 'mcvar', args, 1);
+		
+		if (!args[1]) {
+			return DBot.CommandError('Invalid variable', 'mcvar', args, 2);
+		}
+		
+		let cvar = cvars.Channel(channel).getVar(args[1]);
+		
+		if (!cvar) {
+			return DBot.CommandError('No such variable', 'mcvar', args, 2);
+		}
+		
+		let isPrivate = cvar.haveFlag(FCVAR_PROTECTED);
+		
+		if (isPrivate && !msg.member.hasPermission('MANAGE_GUILD'))
+			return 'Variable have `FCVAR_PROTECTED` flag and you don\'t have `MANAGE_GUILD` permissions! Uh oh! ;n;';
+		
+		if (args[2] === undefined) {
+			if (isPrivate) {
+				msg.author.sendMessage('```' + cvar.format() + '```');
+			} else {
+				msg.channel.sendMessage('```' + cvar.format() + '```');
+			}
+		} else {
+			if (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != DBot.DBot)
+				return 'You must have `MANAGE_GUILD` rights to set channel variables';
+			
+			let trySet = cvar.setValue(args[2]);
+			
+			if (!trySet[0]) {
+				msg.reply(DBot.CommandError(cvars.ErrorMessages[trySet[1]], 'mcvar', args, 3));
+			} else {
+				if (isPrivate) {
+					msg.author.sendMessage('```Channel variable "' + cvar.name + '" has changed to "' + cvar.getFormatedString() + '"```');
+				} else {
+					channel.sendMessage('```Channel variable "' + cvar.name + '" has changed to "' + cvar.getFormatedString() + '"```');
+				}
+			}
+		}
+	}
+});
+
+DBot.RegisterCommand({
 	name: 'uvar',
 	
 	help_args: '<cvar> [new value]',
