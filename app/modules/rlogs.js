17bfcf74481fbc9559f8179c9376563760bbc06c
DBot <roboderpy@gmail.com>
2016-12-07 07:41:27 +0700

    Role logs

--- app/modules/rlogs.js
+++ app/modules/rlogs.js
@@ -6,6 +6,9 @@ const hDuration = require('humanize-duration');
 let updating = {};
 
 let updateRoleRules = function(role) {
+	if (role.name == '@everyone')
+		return;
+	
 	let members = role.members.array();
 	
 	if (updating[role.uid])
@@ -29,7 +32,7 @@ let updateRoleRules = function(role) {
 			}
 			
 			if (!hit) {
-				MySQL.query('INSERT INTO `roles_log` (`MEMBER`, `ROLE`, `TYPE`) VALUES (get_member_id("' + member.id + '", "' + role.guild.id + '"), get_role_id_combined("' + role.id + '", "' + role.guild.id + '"), 1)');
+				MySQL.query('INSERT INTO `roles_log` (`MEMBER`, `ROLE`, `TYPE`, `STAMP`) VALUES (get_member_id("' + member.id + '", "' + role.guild.id + '"), get_role_id_combined("' + role.id + '", "' + role.guild.id + '"), 1, "' + CurTime() + '")');
 				MySQL.query('REPLACE INTO `member_roles` VALUES(get_member_id("' + member.id + '", "' + role.guild.id + '"), get_role_id_combined("' + role.id + '", "' + role.guild.id + '"))');
 			}
 		}
@@ -45,7 +48,7 @@ let updateRoleRules = function(role) {
 			}
 			
 			if (!hit) {
-				MySQL.query('INSERT INTO `roles_log` (`MEMBER`, `ROLE`, `TYPE`) VALUES ("' + row.MEMBER + '", get_role_id_combined("' + role.id + '", "' + role.guild.id + '"), 0)');
+				MySQL.query('INSERT INTO `roles_log` (`MEMBER`, `ROLE`, `TYPE`, `STAMP`) VALUES ("' + row.MEMBER + '", get_role_id_combined("' + role.id + '", "' + role.guild.id + '"), 0, "' + CurTime() + '")');
 				MySQL.query('DELETE FROM `member_roles` WHERE `MEMBER` = "' + row.MEMBER + '" AND `ROLE` = get_role_id_combined("' + role.id + '", "' + role.guild.id + '")');
 			}
 		}
@@ -63,3 +66,97 @@ hook.Add('MemberChanges', 'Default', function(oldM, newM) {
 		updateRoleRules(role);
 	}
 });
+
+/*
+SELECT
+	`roles_log`.`ID` AS `ENTRY`,
+	`roles_log`.`MEMBER`,
+	`roles_log`.`ROLE`,
+	`roles_log`.`TYPE`,
+	`roles_log`.`STAMP`,
+	`roles_names`.`NAME` AS `ROLENAME`,
+	`member_id`.`USER` AS `USERID`,
+	`member_names`.`NAME` AS `MEMBERNAME`
+FROM
+	`roles_log`,
+	`roles_names`,
+	`member_id`,
+	`server_id`,
+	`member_names`
+WHERE
+	`server_id`.`ID` = 6 AND
+	`roles_log`.`MEMBER` = `member_id`.`ID` AND
+	`member_id`.`SERVER` = `server_id`.`ID` AND
+	`roles_names`.`ROLEID` = `roles_log`.`ROLE` AND
+	`member_names`.`ID` = `member_id`.`ID`
+GROUP BY
+	`ENTRY`
+ORDER BY
+	`ENTRY` DESC
+*/
+
+DBot.RegisterCommand({
+	name: 'rolelog',
+	alias: ['rolelogs', 'roleslog', 'roleslogs'],
+	
+	help_args: '',
+	desc: 'Lists some of logs',
+	
+	func: function(args, cmd, msg) {
+		if (DBot.IsPM(msg))
+			return 'Onoh! It is PM ;n;';
+		
+		let funckingQuery = "SELECT\
+			`roles_log`.`ID` AS `ENTRY`,\
+			`roles_log`.`ROLE`,\
+			`roles_log`.`TYPE`,\
+			`roles_log`.`STAMP`,\
+			`roles_names`.`NAME` AS `ROLENAME`,\
+			`member_names`.`NAME` AS `MEMBERNAME`\
+		FROM\
+			`roles_log`,\
+			`roles_names`,\
+			`member_id`,\
+			`server_id`,\
+			`member_names`\
+		WHERE\
+			`server_id`.`ID` = get_server_id('" + msg.channel.guild.id + "') AND\
+			`roles_log`.`MEMBER` = `member_id`.`ID` AND\
+			`member_id`.`SERVER` = `server_id`.`ID` AND\
+			`roles_names`.`ROLEID` = `roles_log`.`ROLE` AND\
+			`member_names`.`ID` = `member_id`.`ID`\
+		GROUP BY\
+			`ENTRY`\
+		ORDER BY\
+			`ENTRY` DESC\
+		LIMIT 0, 10";
+		
+		MySQL.query(funckingQuery, function(err, data) {
+			if (err) {
+				msg.reply('WTF');
+				return;
+			}
+			
+			if (!data || !data[0]) {
+				msg.reply('No data is returned in query');
+				return;
+			}
+			
+			let output = '```\n' + Util.AppendSpaces('User', 20) + Util.AppendSpaces('Role', 10) + Util.AppendSpaces('Type', 5) + Util.AppendSpaces('Time', 30) + '\n';
+			
+			for (let row of data) {
+				let date = moment.unix(row.STAMP);
+				let status = row.TYPE;
+				let name = utf8.decode(row.MEMBERNAME);
+				let rname = utf8.decode(row.ROLENAME);
+				
+				output += Util.AppendSpaces(name, 20) + Util.AppendSpaces(rname, 10) + Util.AppendSpaces(status && 'A' || 'D', 5) + Util.AppendSpaces(date.format('dddd, MMMM Do YYYY, HH:mm:ss') + ' (' + hDuration(Math.floor(CurTime() - row.STAMP) * 1000) + ' ago)', 30) + '\n';
+			}
+			
+			output += '\n```';
+			
+			msg.reply(output);
+		});
+	}
+});
+
