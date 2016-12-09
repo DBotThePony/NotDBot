4d1412d2e1f8c66b06bb104d0cc1f9012de08a9a
DBot <roboderpy@gmail.com>
2016-12-09 18:33:39 +0700

    Removed duplicate

--- app/commands/kill.js
+++ app/commands/kill.js
@@ -53,15 +53,8 @@ const bg = 'rgb(241,233,203)';
 const red = 'rgb(163,87,74)';
 const blu = 'rgb(85,124,131)';
 
-module.exports = {
-	name: 'kill',
-	alias: ['killicon', 'killfeed'],
-	
-	help_args: '<user 1 (@User or text)> <weapon> [User2]',
-	desc: 'Generates TF2 kill text',
-	allowUserArgument: true,
-	
-	func: function(args, cmd, msg) {
+let generateFunc = function(col1, col2) {
+	return function(args, cmd, msg) {
 		if (!args[0])
 			return DBot.CommandError('No user', 'kill', args, 1);
 		
@@ -119,14 +112,14 @@ module.exports = {
 					'canvas:' + bg,
 					'-font', Font,
 					'-pointsize', FontSize,
-					'-fill', red,
+					'-fill', col1,
 					'-weight', '700',
 				];
 				
 				if (username2) {
 					magikArgs.push('-draw', Util.MonospaceText(40, 50, username, FontSize, FontSpacing));
 					magikArgs.push('-draw', 'image over ' + (calcWidthFirst - 10 + width / 2) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-fill', blu, '-draw', Util.MonospaceText(35 + calcWidthFirst + width, 50, username2, FontSize, FontSpacing));
+					magikArgs.push('-fill', col2, '-draw', Util.MonospaceText(35 + calcWidthFirst + width, 50, username2, FontSize, FontSpacing));
 				} else {
 					magikArgs.push('-draw', 'image over 40,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
 					magikArgs.push('-draw', Util.MonospaceText(60 + width, 50, username, FontSize, FontSpacing));
@@ -148,7 +141,18 @@ module.exports = {
 				});
 			});
 		});
-	}
+	};
+}
+
+module.exports = {
+	name: 'kill',
+	alias: ['killicon', 'killfeed'],
+	
+	help_args: '<user 1 (@User or text)> <weapon> [User2]',
+	desc: 'Generates TF2 kill text',
+	allowUserArgument: true,
+	
+	func: generateFunc(red, blu),
 }
 
 DBot.RegisterCommand({
@@ -159,94 +163,7 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. Instead of RED / BLU uses BLU / RED',
 	allowUserArgument: true,
 	
-	func: function(args, cmd, msg) {
-		if (!args[0])
-			return DBot.CommandError('No user', 'kill', args, 1);
-		
-		let username = args[0];
-		
-		if (typeof username == 'object')
-			username = username.username;
-		
-		if (!args[1])
-			return DBot.CommandError('No weapon name', 'kill', args, 1);
-		
-		let weapon = args[1].toLowerCase();
-		
-		let username2 = args[2];
-		
-		if (typeof username2 == 'object')
-			username2 = username2.username;
-		
-		let sha = DBot.HashString(username + ' ' + weapon + ' ' + username2);
-		let fpath = DBot.WebRoot + '/killfeed/' + sha + '_b.png';
-		let fpathU = DBot.URLRoot + '/killfeed/' + sha + '_b.png';
-		
-		fs.stat(fpath, function(err, stat) {
-			if (stat) {
-				msg.reply(fpathU);
-				return;
-			}
-			
-			MySQL.query('SELECT * FROM killicons WHERE "CLASSNAME" = ' + Util.escape(weapon) + ' OR "NAME" = ' + Util.escape(weapon) + ' OR "NAME" LIKE ' + Util.escape('%' + weapon + '%') + ' OR "CLASSNAME" LIKE ' + Util.escape('%' + weapon + '%'), function(err, data) {
-				if (err) {
-					msg.reply('<internal pony error>');
-					return;
-				}
-				
-				if (!data || !data[0]) {
-					msg.reply('No such weapon');
-					return;
-				}
-				
-				let filename = data[0].FILENAME;
-				
-				let width = Number(data[0].WIDTH);
-				let iheight = Number(data[0].HEIGHT);
-				let height = 70;
-				let calcWidthFirst = (username.length + 1) * FontSize * FontSpacing;
-				let calcWidthLast = 0;
-				
-				if (username2)
-					calcWidthLast = (username2.length + 1) * FontSize * FontSpacing + 50;
-				
-				let totalWidth = calcWidthFirst + width + calcWidthLast - 30;
-				
-				let magikArgs = [
-					'-size', totalWidth + 'x' + height,
-					'canvas:' + bg,
-					'-font', Font,
-					'-pointsize', FontSize,
-					'-fill', blu,
-					'-weight', '700',
-				];
-				
-				if (username2) {
-					magikArgs.push('-draw', Util.MonospaceText(40, 50, username, FontSize, FontSpacing));
-					magikArgs.push('-draw', 'image over ' + (calcWidthFirst + 10 + width / 2) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-fill', red, '-draw', Util.MonospaceText(35 + calcWidthFirst + width, 50, username2, FontSize, FontSpacing));
-				} else {
-					magikArgs.push('-draw', 'image over 40,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', Util.MonospaceText(60 + width, 50, username, FontSize, FontSpacing));
-				}
-				
-				magikArgs.push(fpath);
-				
-				let magik = spawn('convert', magikArgs);
-				
-				Util.Redirect(magik);
-				
-				magik.on('close', function(code) {
-					if (code != 0) {
-						msg.reply('<internal pony error>');
-						return;
-					}
-					
-					msg.reply(fpathU);
-				});
-			});
-		});
-	}
+	func: generateFunc(blu, red),
 });
 
 DBot.RegisterCommand({
@@ -257,94 +174,7 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. TEAM KILLER!',
 	allowUserArgument: true,
 	
-	func: function(args, cmd, msg) {
-		if (!args[0])
-			return DBot.CommandError('No user', 'kill', args, 1);
-		
-		let username = args[0];
-		
-		if (typeof username == 'object')
-			username = username.username;
-		
-		if (!args[1])
-			return DBot.CommandError('No weapon name', 'kill', args, 1);
-		
-		let weapon = args[1].toLowerCase();
-		
-		let username2 = args[2];
-		
-		if (typeof username2 == 'object')
-			username2 = username2.username;
-		
-		let sha = DBot.HashString(username + ' ' + weapon + ' ' + username2);
-		let fpath = DBot.WebRoot + '/killfeed/' + sha + '_b.png';
-		let fpathU = DBot.URLRoot + '/killfeed/' + sha + '_b.png';
-		
-		fs.stat(fpath, function(err, stat) {
-			if (stat) {
-				msg.reply(fpathU);
-				return;
-			}
-			
-			MySQL.query('SELECT * FROM killicons WHERE "CLASSNAME" = ' + Util.escape(weapon) + ' OR "NAME" = ' + Util.escape(weapon) + ' OR "NAME" LIKE ' + Util.escape('%' + weapon + '%') + ' OR "CLASSNAME" LIKE ' + Util.escape('%' + weapon + '%'), function(err, data) {
-				if (err) {
-					msg.reply('<internal pony error>');
-					return;
-				}
-				
-				if (!data || !data[0]) {
-					msg.reply('No such weapon');
-					return;
-				}
-				
-				let filename = data[0].FILENAME;
-				
-				let width = Number(data[0].WIDTH);
-				let iheight = Number(data[0].HEIGHT);
-				let height = 70;
-				let calcWidthFirst = (username.length + 1) * FontSize * FontSpacing;
-				let calcWidthLast = 0;
-				
-				if (username2)
-					calcWidthLast = (username2.length + 1) * FontSize * FontSpacing + 50;
-				
-				let totalWidth = calcWidthFirst + width + calcWidthLast - 30;
-				
-				let magikArgs = [
-					'-size', totalWidth + 'x' + height,
-					'canvas:' + bg,
-					'-font', Font,
-					'-pointsize', FontSize,
-					'-fill', red,
-					'-weight', '700',
-				];
-				
-				if (username2) {
-					magikArgs.push('-draw', Util.MonospaceText(40, 50, username, FontSize, FontSpacing));
-					magikArgs.push('-draw', 'image over ' + (calcWidthFirst + 10 + width / 2) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', Util.MonospaceText(35 + calcWidthFirst + width, 50, username2, FontSize, FontSpacing));
-				} else {
-					magikArgs.push('-draw', 'image over 40,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', Util.MonospaceText(60 + width, 50, username, FontSize, FontSpacing));
-				}
-				
-				magikArgs.push(fpath);
-				
-				let magik = spawn('convert', magikArgs);
-				
-				Util.Redirect(magik);
-				
-				magik.on('close', function(code) {
-					if (code != 0) {
-						msg.reply('<internal pony error>');
-						return;
-					}
-					
-					msg.reply(fpathU);
-				});
-			});
-		});
-	}
+	func: generateFunc(red, red),
 });
 
 DBot.RegisterCommand({
@@ -355,92 +185,5 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. BLU TEAM KILLER!',
 	allowUserArgument: true,
 	
-	func: function(args, cmd, msg) {
-		if (!args[0])
-			return DBot.CommandError('No user', 'kill', args, 1);
-		
-		let username = args[0];
-		
-		if (typeof username == 'object')
-			username = username.username;
-		
-		if (!args[1])
-			return DBot.CommandError('No weapon name', 'kill', args, 1);
-		
-		let weapon = args[1].toLowerCase();
-		
-		let username2 = args[2];
-		
-		if (typeof username2 == 'object')
-			username2 = username2.username;
-		
-		let sha = DBot.HashString(username + ' ' + weapon + ' ' + username2);
-		let fpath = DBot.WebRoot + '/killfeed/' + sha + '_b.png';
-		let fpathU = DBot.URLRoot + '/killfeed/' + sha + '_b.png';
-		
-		fs.stat(fpath, function(err, stat) {
-			if (stat) {
-				msg.reply(fpathU);
-				return;
-			}
-			
-			MySQL.query('SELECT * FROM killicons WHERE "CLASSNAME" = ' + Util.escape(weapon) + ' OR "NAME" = ' + Util.escape(weapon) + ' OR "NAME" LIKE ' + Util.escape('%' + weapon + '%') + ' OR "CLASSNAME" LIKE ' + Util.escape('%' + weapon + '%'), function(err, data) {
-				if (err) {
-					msg.reply('<internal pony error>');
-					return;
-				}
-				
-				if (!data || !data[0]) {
-					msg.reply('No such weapon');
-					return;
-				}
-				
-				let filename = data[0].FILENAME;
-				
-				let width = Number(data[0].WIDTH);
-				let iheight = Number(data[0].HEIGHT);
-				let height = 70;
-				let calcWidthFirst = (username.length + 1) * FontSize * FontSpacing;
-				let calcWidthLast = 0;
-				
-				if (username2)
-					calcWidthLast = (username2.length + 1) * FontSize * FontSpacing + 50;
-				
-				let totalWidth = calcWidthFirst + width + calcWidthLast - 30;
-				
-				let magikArgs = [
-					'-size', totalWidth + 'x' + height,
-					'canvas:' + bg,
-					'-font', Font,
-					'-pointsize', FontSize,
-					'-fill', blu,
-					'-weight', '700',
-				];
-				
-				if (username2) {
-					magikArgs.push('-draw', Util.MonospaceText(40, 50, username, FontSize, FontSpacing));
-					magikArgs.push('-draw', 'image over ' + (calcWidthFirst + 10 + width / 2) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', Util.MonospaceText(35 + calcWidthFirst + width, 50, username2, FontSize, FontSpacing));
-				} else {
-					magikArgs.push('-draw', 'image over 40,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', Util.MonospaceText(60 + width, 50, username, FontSize, FontSpacing));
-				}
-				
-				magikArgs.push(fpath);
-				
-				let magik = spawn('convert', magikArgs);
-				
-				Util.Redirect(magik);
-				
-				magik.on('close', function(code) {
-					if (code != 0) {
-						msg.reply('<internal pony error>');
-						return;
-					}
-					
-					msg.reply(fpathU);
-				});
-			});
-		});
-	}
+	func: generateFunc(blu, blu),
 });
