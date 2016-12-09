d1614607500be94cc48888842e9b44d20c4efbf2
DBot <roboderpy@gmail.com>
2016-12-09 15:26:38 +0700

    Purge command

--- app/commands/wipe.js
+++ app/commands/wipe.js
@@ -102,3 +102,88 @@ module.exports = {
 		conf.echo();
 	},
 }
+
+DBot.RegisterCommand({
+	name: 'purge',
+	
+	help_args: '<amount>',
+	desc: 'Wipes messages in current channel\nI need MANAGE_MESSAGES permissions to do that!\nSimilar to wipe command, but deletes always all messages\nand have adjustable limit',
+	
+	func: function(args, cmd, msg) {
+		if (DBot.IsPM(msg))
+			return 'Must execute in server channel ;n;';
+		
+		let amount = Util.ToNumber(args[0]) || 30;
+		let me = DBot.FindMeInChannel(msg.channel);
+		
+		if (!me)
+			return '<internal pony error>';
+		
+		if (!msg.member.hasPermission('MANAGE_MESSAGES'))
+			return 'You must have MANAGE_MESSAGES permission!';
+		
+		if (!me.hasPermission('MANAGE_MESSAGES'))
+			return 'I must have MANAGE_MESSAGES permission ;n;';
+		
+		var conf = new DBot.Confirm(msg.author, msg.channel);
+		
+		conf.setTitle('Purge of messages');
+		conf.setDesc('Delete last ' + amount + ' messages');
+		
+		conf.confirm(function() {
+			conf.clearMessages();
+			
+			msg.channel.fetchMessages({limit: 100})
+			.then(function() {
+				var messages = msg.channel.messages;
+				var arr = messages.array();
+				if (!arr[0]) {
+					msg.channel.sendMessage('No messages to remove? WTF');
+					return;
+				}
+				
+				var filtered = [];
+				
+				for (var i in arr) {
+					if (!arr[i].deletable)
+						continue;
+					
+					filtered.push(arr[i]);
+					
+					if (filtered.length > amount)
+						break;
+				}
+				
+				if (!filtered[0]) {
+					msg.channel.sendMessage('No messages matched to be removed.');
+					return;
+				}
+				
+				var continueDelete = function() {
+					for (var i in filtered) {
+						if (i == 0)
+							continue;
+						
+						filtered[i].delete(0);
+					}
+				}
+				
+				filtered[0].delete(0).then(continueDelete)
+				.catch(function(mess) {
+					if (mess.status == 404) {
+						continueDelete();
+						return;
+					}
+					
+					msg.channel.sendMessage('Can\'t delete ;n;');
+				});
+			});
+		});
+		
+		conf.decline(function() {
+			msg.reply('Aborting');
+		});
+		
+		conf.echo();
+	},
+});
