bc87a1d88913d878cf818889308a9559e8a96bfc
DBot <roboderpy@gmail.com>
2016-12-10 13:36:58 +0700

    Updated magik command

--- app/commands/magik.js
+++ app/commands/magik.js
@@ -52,33 +52,32 @@ module.exports = {
 			}
 		}
 		
-		var hash = DBot.HashString(url);
-		var uObj = URL.parse(url);
-		var path = uObj.pathname;
-		var split = path.split('.');
-		var ext = split[split.length - 1].toLowerCase();
+		let hash = DBot.HashString(url);
+		let uObj = URL.parse(url);
+		let path = uObj.pathname;
+		let split = path.split('.');
+		let ext = split[split.length - 1].toLowerCase();
 		
 		if (!DBot.HaveValue(allowed, ext))
 			return 'Invalid url maybe? ;w;' + Util.HighlightHelp(['magik'], 2, args);
 		
-		var comb1 = DBot.RandomArray(combinations);
-		var comb2 = DBot.RandomArray(combinations);
+		let comb1 = DBot.RandomArray(combinations);
+		let comb2 = DBot.RandomArray(combinations);
 		
-		var selectedDimensions = comb1[0] + '%x' + comb2[0] + '%';
-		var selectedDimensions2 = comb1[1] + '%x' + comb2[1] + '%';
+		let selectedDimensions = comb1[0] + '%x' + comb2[0] + '%';
+		let selectedDimensions2 = comb1[1] + '%x' + comb2[1] + '%';
 		
-		var fPath;
+		let fPath;
 		
-		var newHash = DBot.HashString(url + ' ' + selectedDimensions);
+		let newHash = DBot.HashString(url + ' ' + selectedDimensions);
 		
-		var fPathProcessedTmp = DBot.WebRoot + '/magik/' + newHash + '_tmp.png';
-		var fPathProcessed = DBot.WebRoot + '/magik/' + newHash + '.png';
-		var fPathProcessedURL = DBot.URLRoot + '/magik/' + newHash + '.png';
+		let fPathProcessed = DBot.WebRoot + '/magik/' + newHash + '.png';
+		let fPathProcessedURL = DBot.URLRoot + '/magik/' + newHash + '.png';
 		
 		msg.channel.startTyping();
 		
-		var msgNew;
-		var iShouldDelete = false;
+		let msgNew;
+		let iShouldDelete = false;
 		
 		msg.oldReply(DBot.GenerateWaitMessage()).then(function(i) {
 			msgNew = i;
@@ -87,7 +86,7 @@ module.exports = {
 				msgNew.delete(0);
 		});
 		
-		var ContinueFunc = function() {
+		let ContinueFunc = function() {
 			fs.stat(fPathProcessed, function(err, stat) {
 				if (stat && stat.isFile()) {
 					msg.channel.stopTyping();
@@ -97,39 +96,22 @@ module.exports = {
 					
 					msg.reply(fPathProcessedURL);
 				} else {
-					var magik = spawn('convert', ['(', fPath, '-resize', '2000x2000>', ')', '-liquid-rescale', selectedDimensions, fPathProcessedTmp]);
+					let magik = spawn('convert', ['(', '(', fPath, '-resize', '2000x2000>', ')', '-liquid-rescale', selectedDimensions, ')', '-resize', selectedDimensions2, fPathProcessed]);
 					
 					magik.stderr.on('data', function(data) {
 						console.error(data.toString());
 					});
 					
 					magik.on('close', function(code) {
+						iShouldDelete = true;
+						if (msgNew)
+							msgNew.delete(0);
+						
 						if (code == 0) {
-							var magik = spawn('convert', [fPathProcessedTmp, '-resize', selectedDimensions2, fPathProcessed]);
-							
-							magik.stderr.on('data', function(data) {
-								console.error(data.toString());
-							});
-							
-							magik.on('close', function(code) {
-								iShouldDelete = true;
-								if (msgNew)
-									msgNew.delete(0);
-								
-								msg.channel.stopTyping();
-								
-								if (code == 0) {
-									msg.reply(fPathProcessedURL);
-								} else {
-									msg.reply('Uh oh! You are trying to break me ;n; Why? ;n;');
-								}
-							});
+							msg.channel.stopTyping();
+							msg.reply(fPathProcessedURL);
 						} else {
 							msg.channel.stopTyping();
-							iShouldDelete = true;
-							if (msgNew)
-								msgNew.delete(0);
-							
 							msg.reply('Uh oh! You are trying to break me ;n; Why? ;n;');
 						}
 					});
