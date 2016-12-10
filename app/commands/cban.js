d324ce82ef21349a5f29b413319a9ff0c813a12c
DBot <roboderpy@gmail.com>
2016-12-10 19:41:01 +0700

    Improved command ban commands

--- app/commands/cban.js
+++ app/commands/cban.js
@@ -8,133 +8,84 @@ module.exports = {
 	allowUserArgument: true,
 	
 	func: function(args, cmd, msg) {
-		if (DBot.IsPM(msg)) {
+		if (DBot.IsPM(msg))
 			return 'Onoh! This is PM channel! ;n;';
-		}
+		
+		if (!msg.member.hasPermission('MANAGE_CHANNELS') && msg.author.id != DBot.DBot)
+			return 'Onoh! You must have at least `MANAGE_CHANNELS` permission to command me to do that :s';
 		
 		let realm = args[0];
 		let command = args[1];
 		
 		if (!realm || realm != 'server' && realm != 'channel' && realm != 'member')
-			return 'Realm must be server, channel or member';
+			return DBot.CommandError('Realm must be channel, server or member', 'cban', args, 1);
 		
-		if (realm == 'member')
+		if (realm == 'member') {
 			command = args[2];
+			
+			if (!command || !DBot.Commands[command])
+				return DBot.CommandError('No such command', 'cban', args, 3);
+		} else {
+			if (!command || !DBot.Commands[command])
+				return DBot.CommandError('No such command', 'cban', args, 2);
+		}
 		
-		if (!command || !DBot.Commands[command])
-			return 'Unknown command to ban ;w;';
+		let cBans;
+		let getUser;
 		
 		if (realm == 'server') {
-			if (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_GUILD` permission to command me to do that :s';
-			
-			let cBans = DBot.ServerCBans(msg.channel.guild);
-			
-			let success = [];
-			let fail = [];
-			
-			for (i = 1; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.ban(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
-			}
-			
-			return '\nBanned commands on server: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
+			cBans = DBot.ServerCBans(msg.channel.guild);
 		} else if (realm == 'member') {
-			if (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_GUILD` permission to command me to do that :s';
-			
 			if (typeof args[1] != 'object')
 				return DBot.CommandError('Must be an @User', 'cban', args, 2);
 			
-			let getUser = msg.channel.guild.member(args[1]);
+			getUser = msg.channel.guild.member(args[1]);
 			
 			if (!getUser)
 				return DBot.CommandError('Must be an @User', 'cban', args, 2);
 			
-			let cBans = DBot.MemberCBans(getUser);
-			
-			let success = [];
-			let fail = [];
-			
-			for (i = 2; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.ban(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
-			}
-			
-			return '\nBanned commands from user @' + (getUser.nickname || getUser.user.username) + ': ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
+			cBans = DBot.MemberCBans(getUser);
 		} else {
-			if (!msg.member.hasPermission('MANAGE_CHANNELS') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_CHANNELS` permission to command me to do that :s';
-			
 			let cBans = DBot.ChannelCBans(msg.channel);
+		}
+		
+		let success = [];
+		let fail = [];
+		let startI = 1;
+		
+		if (realm == 'member')
+			startI = 2;
+		
+		for (i = startI; i < args.length; i++) {
+			let id = args[i].toLowerCase();
+			let data = DBot.Commands[id];
 			
-			let success = [];
-			let fail = [];
-			
-			for (i = 1; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.ban(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
+			if (!data) {
+				fail.push(id);
+				continue;
 			}
 			
-			return '\nBanned commands on channel: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
+			if (data.id != id && data.name != id) {
+				id = data.id;
+			}
+			
+			if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
+				let status = cBans.ban(id);
+				
+				if (status)
+					success.push(id);
+				else
+					fail.push(id);
+			} else
+				fail.push(id);
 		}
+		
+		if (realm == 'server')
+			return '\nBanned commands on server: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
+		else if (realm == 'member')
+			return '\nBanned commands from user @' + (getUser.nickname || getUser.user.username) + ': ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
+		else
+			return '\nBanned commands on channel: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
 	},
 }
 
@@ -147,133 +98,84 @@ DBot.RegisterCommand({
 	allowUserArgument: true,
 	
 	func: function(args, cmd, msg) {
-		if (DBot.IsPM(msg)) {
-			return 'Onoh! This is PM channel!';
-		}
+		if (DBot.IsPM(msg))
+			return 'Onoh! This is PM channel! ;n;';
+		
+		if (!msg.member.hasPermission('MANAGE_CHANNELS') && msg.author.id != DBot.DBot)
+			return 'Onoh! You must have at least `MANAGE_CHANNELS` permission to command me to do that :s';
 		
 		let realm = args[0];
 		let command = args[1];
 		
 		if (!realm || realm != 'server' && realm != 'channel' && realm != 'member')
-			return 'Realm must be server, channel or member';
+			return DBot.CommandError('Realm must be channel, server or member', 'cban', args, 1);
 		
-		if (realm == 'member')
+		if (realm == 'member') {
 			command = args[2];
+			
+			if (!command || !DBot.Commands[command])
+				return DBot.CommandError('No such command', 'cban', args, 3);
+		} else {
+			if (!command || !DBot.Commands[command])
+				return DBot.CommandError('No such command', 'cban', args, 2);
+		}
 		
-		if (!command || !DBot.Commands[command])
-			return 'Unknown command to ban ;w;';
+		let cBans;
+		let getUser;
 		
 		if (realm == 'server') {
-			if (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_GUILD` permission to command me to do that :s';
-			
-			let cBans = DBot.ServerCBans(msg.channel.guild);
-			
-			let success = [];
-			let fail = [];
-			
-			for (i = 1; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.unBan(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
-			}
-			
-			return '\nUnbanned commands on server: ' + DBot.ConcatArray(success, ', ') + '\nFailed to ban: ' + DBot.ConcatArray(fail, ', ') + '\nIf there is failures - that means command already unbanned, or not allowed to be unbanned!';
+			cBans = DBot.ServerCBans(msg.channel.guild);
 		} else if (realm == 'member') {
-			if (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_GUILD` permission to command me to do that :s';
-			
 			if (typeof args[1] != 'object')
-				return DBot.CommandError('Must be an @User', 'cuban', args, 2);
+				return DBot.CommandError('Must be an @User', 'cban', args, 2);
 			
-			let getUser = msg.channel.guild.member(args[1]);
+			getUser = msg.channel.guild.member(args[1]);
 			
 			if (!getUser)
-				return DBot.CommandError('Must be an @User', 'cuban', args, 2);
+				return DBot.CommandError('Must be an @User', 'cban', args, 2);
 			
-			let cBans = DBot.MemberCBans(getUser);
-			
-			let success = [];
-			let fail = [];
-			
-			for (i = 2; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.unBan(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
-			}
-			
-			return '\nBanned commands from user @' + (getUser.nickname || getUser.user.username) + ': ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already banned, or not allowed to be banned!';
-		}  else {
-			if (!msg.member.hasPermission('MANAGE_CHANNELS') && msg.author.id != DBot.DBot)
-				return 'Onoh! You must have at least `MANAGE_CHANNELS` permission to command me to do that :s';
-			
+			cBans = DBot.MemberCBans(getUser);
+		} else {
 			let cBans = DBot.ChannelCBans(msg.channel);
+		}
+		
+		let success = [];
+		let fail = [];
+		let startI = 1;
+		
+		if (realm == 'member')
+			startI = 2;
+		
+		for (i = startI; i < args.length; i++) {
+			let id = args[i].toLowerCase();
+			let data = DBot.Commands[id];
 			
-			let success = [];
-			let fail = [];
-			
-			for (i = 1; i < args.length; i++) {
-				let id = args[i].toLowerCase();
-				let data = DBot.Commands[id];
-				
-				if (!data) {
-					fail.push(id);
-					continue;
-				}
-				
-				if (data.id != id && data.name != id) {
-					id = data.id;
-				}
-				
-				if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
-					let status = cBans.unBan(id);
-					
-					if (status)
-						success.push(id);
-					else
-						fail.push(id);
-				} else
-					fail.push(id);
+			if (!data) {
+				fail.push(id);
+				continue;
 			}
 			
-			return '\nUnbanned commands on channel: ' + DBot.ConcatArray(success, ', ') + '\nFailed to ban: ' + DBot.ConcatArray(fail, ', ') + '\nIf there is failures - that means command already unbanned, or not allowed to be unbanned!';
+			if (data.id != id && data.name != id) {
+				id = data.id;
+			}
+			
+			if (!DBot.HaveValue(DBot.DisallowCommandManipulate, id)) {
+				let status = cBans.unBan(id);
+				
+				if (status)
+					success.push(id);
+				else
+					fail.push(id);
+			} else
+				fail.push(id);
 		}
+		
+		if (realm == 'server')
+			return '\nUnbanned commands on server: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already not banned, or not allowed to be unbanned!';
+		else if (realm == 'member')
+			return '\nUnbanned commands from user @' + (getUser.nickname || getUser.user.username) + ': ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already not banned, or not allowed to be unbanned!';
+		else
+			return '\nUnbanned commands on channel: ' + success.join(', ') + '\nFailed to ban: ' + fail.join(', ') + '\nIf there is failures - that means command already not banned, or not allowed to be unbanned!';
 	},
 });
 
@@ -297,7 +199,7 @@ DBot.RegisterCommand({
 			let output = '\nCommands banned on this server:\n';
 			
 			if (cBans1.bans[0]) {
-				output += '```' + DBot.ConcatArray(cBans1.bans, ', ') + '```\n';
+				output += '```' + cBans1.bans.join(', ') + '```\n';
 			} else {
 				output += '<No bans>\n';
 			}
@@ -305,7 +207,7 @@ DBot.RegisterCommand({
 			output += 'Commands banned on this channel:\n'
 			
 			if (cBans2.bans[0]) {
-				output += '```' + DBot.ConcatArray(cBans2.bans, ', ') + '```\n';
+				output += '```' + cBans2.bans.join(', ') + '```\n';
 			} else {
 				output += '<No bans>\n';
 			}
