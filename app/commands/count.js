f60f8a306ef72df011fb74bb5210b9134e3cada4
DBot <roboderpy@gmail.com>
2016-12-10 13:01:49 +0700

    Count command example

---
+++ app/commands/count.js
@@ -0,0 +1,235 @@
+
+let Actions = {
+	roles: function(args, cmd, msg) {
+		if (DBot.IsPM(msg))
+			return DBot.CommandError('PM? ;n;', 'count', args, 1);
+		
+		if (!args[1]) {
+			let total = 0;
+			
+			for (let server of DBot.bot.guilds.array()) {
+				total += server.roles.array().length;
+			}
+			
+			return 'Total roles on this server: ' + msg.channel.guild.roles.array().length + '\nTotal roles on all server: ' + total;
+		} else {
+			if (args[1] == 'list') {
+				let names = [];
+				
+				for (let role of msg.channel.guild.roles.array()) {
+					names.push(role.name);
+				}
+				
+				return '```' + names.join(', ') + '```';
+			} else if (args[1] == 'total' || args[1] == 'all') {
+				Postgres.query('SELECT COUNT(*) AS "COUNT" FROM roles_id', function(err, data) {
+					if (err) {
+						msg.reply('Something went wrong ;n;');
+						return;
+					}
+					
+					msg.reply('I totally remember **' + data[0].COUNT + '** unqiue roles!');
+				});
+			} else if (args[1] == 'server' && args[2] && (args[2] == 'total' || args[2] == 'all')) {
+				Postgres.query('SELECT COUNT(*) AS "COUNT" FROM roles_id WHERE "SERVER" = ' + sql.Server(msg.channel.guild), function(err, data) {
+					if (err) {
+						msg.reply('Something went wrong ;n;');
+						return;
+					}
+					
+					msg.reply('I totally remember **' + data[0].COUNT + '** unqiue roles on this server!');
+				});
+			} else if (args[1] == 'users') {
+				let tags = [];
+				
+				for (let role of msg.channel.guild.roles.array()) {
+					tags.push([role.name, role.members.array().length]);
+				}
+				
+				tags.sort(function(a, b) {
+					if (a[1] < b[1]) {
+						return 1;
+					} else {
+						return -1;
+					}
+					
+					return 0;
+				});
+				
+				let output = '```\n';
+				
+				for (let i = 0; i < 10; i++) {
+					if (!tags[i])
+						break;
+					
+					output += Util.AppendSpaces(tags[i][0], 20) + tags[i][1] + ' Users\n';
+				}
+				
+				return output + '```';
+			} else {
+				return DBot.CommandError('Unknown subcommand', 'count', args, 2);
+			}
+		}
+	},
+	
+	servers: function(args, cmd, msg) {
+		if (!args[1]) {
+			return 'Totally running on **' + DBot.bot.guilds.array().length + '** servers!';
+		} else if (args[1] == 'total' || args[1] == 'all') {
+			Postgres.query('SELECT COUNT(*) AS "COUNT" FROM server_id', function(err, data) {
+				if (err) {
+					msg.reply('Something went wrong ;n;');
+					return;
+				}
+				
+				msg.reply('I totally remember **' + data[0].COUNT + '** servers!');
+			});
+		} else {
+			return DBot.CommandError('Unknown subcommand', 'count', args, 2);
+		}
+	},
+	
+	channels: function(args, cmd, msg) {
+		if (!args[1]) {
+			let num = 0;
+			
+			for (let server of DBot.bot.guilds.array()) {
+				num += server.channels.array().length;
+			}
+			
+			return 'Totally running on **' + num + '** channels!';
+		} else if (args[1] == 'total' || args[1] == 'all') {
+			Postgres.query('SELECT COUNT(*) AS "COUNT" FROM channel_id', function(err, data) {
+				if (err) {
+					msg.reply('Something went wrong ;n;');
+					return;
+				}
+				
+				msg.reply('I totally remember **' + data[0].COUNT + '** channels!');
+			});
+		} else {
+			return DBot.CommandError('Unknown subcommand', 'count', args, 2);
+		}
+	},
+	
+	users: function(args, cmd, msg) {
+		if (!args[1]) {
+			let num = 0;
+			
+			for (let server of DBot.bot.guilds.array()) {
+				num += server.members.array().length;
+			}
+			
+			return 'Serving **' + num + '** members. To get unique amount of members, type `count users unique`';
+		} else {
+			if (args[1] == 'unique') {
+				return 'Serving ' + DBot.GetUsers().length + ' unique members!';
+			} else if (args[1] == 'all' || args[1] == 'total') {
+				Postgres.query('SELECT COUNT(*) AS "COUNT" FROM user_id', function(err, data) {
+					if (err) {
+						msg.reply('Something went wrong ;n;');
+						return;
+					}
+					
+					msg.reply('I totally remember **' + data[0].COUNT + '** users!');
+				});
+			} else if (args[1] == 'members' && args[2] && (args[2] == 'all' || args[2] == 'total')) {
+				Postgres.query('SELECT COUNT(*) AS "COUNT" FROM member_id', function(err, data) {
+					if (err) {
+						msg.reply('Something went wrong ;n;');
+						return;
+					}
+					
+					msg.reply('I totally remember **' + data[0].COUNT + '** unique members! (user-server combination)');
+				});
+			} else if (args[1] == 'online') {
+				if (args[2] && args[2].toLowerCase() == 'explicit') {
+					let num = 0;
+					let usrs = 0;
+					
+					for (let user of DBot.GetUsers()) {
+						usrs++;
+						if (user.presence.status == 'online')
+							num++;
+					}
+					
+					return 'Serving **' + usrs + '** unique users, online users: **' + num + '**';
+				} else {
+					let num = 0;
+					let usrs = 0;
+					
+					for (let user of DBot.GetUsers()) {
+						usrs++;
+						if (user.presence.status != 'offline')
+							num++;
+					}
+					
+					return 'Serving **' + usrs + '** unique users, non-offline users: **' + num + '**';
+				}
+			} else if (args[1] == 'offline') {
+				let num = 0;
+				let usrs = 0;
+				
+				for (let user of DBot.GetUsers()) {
+					usrs++;
+					if (user.presence.status == 'offline')
+						num++;
+				}
+				
+				return 'Serving **' + usrs + '** unique users, offline users: **' + num + '**';
+			} else if (args[1] == 'away' || args[1] == 'idle') {
+				let num = 0;
+				let usrs = 0;
+				
+				for (let user of DBot.GetUsers()) {
+					usrs++;
+					if (user.presence.status == 'idle')
+						num++;
+				}
+				
+				return 'Serving **' + usrs + '** unique users, AFK users: **' + num + '**';
+			} else if (args[1] == 'dnd') {
+				let num = 0;
+				let usrs = 0;
+				
+				for (let user of DBot.GetUsers()) {
+					usrs++;
+					if (user.presence.status == 'dnd')
+						num++;
+				}
+				
+				return 'Serving **' + usrs + '** unique users, DND users: **' + num + '**';
+			} else {
+				return DBot.CommandError('Unknown subcommand', 'count', args, 2);
+			}
+		}
+	},
+	
+	
+}
+
+let help = ``;
+
+module.exports = {
+	name: 'count',
+	
+	help_args: '<action> ...',
+	desc: 'Displays some statistics',
+	desc_full: help,
+	allowUserArgument: true,
+	
+	func: function(args, cmd, msg) {
+		if (!args[0])
+			return DBot.CommandError('No action specified', 'count', args, 1);
+		
+		if (!Actions[args[0].toLowerCase()])
+			return DBot.CommandError('No such action', 'count', args, 1);
+		
+		if (args[1])
+			args[1] = args[1].toLowerCase();
+		
+		let func = Actions[args[0].toLowerCase()];
+		
+		return func(args, cmd, msg);
+	}
+}
