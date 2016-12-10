773bddc822d7800d866ef45d213e22c5ec4b0a2e
DBot <roboderpy@gmail.com>
2016-12-10 08:10:19 +0700

    Fixed save/load

--- app/lib/imagick.js
+++ app/lib/imagick.js
@@ -14,8 +14,8 @@ ImageMagik = IMagick;
 
 const Match = /Font: ([^\r\n]+)/;
 const MatchGlobal = /Font: ([^\r\n]+)/gi;
-const CharsToCheckForSize = '`1234567890-=~!@#$%^&*()_+qwertyuiop[]asdfghjkl\'\\zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?|ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю"№;';
-const CharsExprStr = '`|1|2|3|4|5|6|7|8|9|0|-|=|~|\\!|@|#|\\$|%|\\^|\\&|\\*|\\(|\\)|_|\\+|q|w|e|r|t|y|u|i|o|p|[|]|a|s|d|f|g|h|j|k|l|\'|\\|z|x|c|v|b|n|m|,|.|/|Q|W|E|R|T|Y|U|I|O|P|{|}|A|S|D|F|G|H|J|K|L|:|Z|X|C|V|B|N|M|<|>|\\?|\\||Й|Ц|У|К|Е|Н|Г|Ш|Щ|З|Х|Ъ|Ф|Ы|В|А|П|Р|О|Л|Д|Ж|Э|Я|Ч|С|М|И|Т|Ь|Б|Ю|й|ц|у|к|е|н|г|ш|щ|з|х|ъ|ф|ы|в|а|п|р|о|л|д|ж|э|я|ч|с|м|и|т|ь|б|ю|"|№|;';
+const CharsToCheckForSize = '`1234567890-=~!@#$%^&*()_+qwertyuiop[]asdfghjkl\'\\zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?|ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю"№; ';
+const CharsExprStr = '`|1|2|3|4|5|6|7|8|9|0|-|=|~|\\!|@|#|\\$|%|\\^|\\&|\\*|\\(|\\)|_|\\+|q|w|e|r|t|y|u|i|o|p|[|]|a|s|d|f|g|h|j|k|l|\'|\\|z|x|c|v|b|n|m|,|.|/|Q|W|E|R|T|Y|U|I|O|P|{|}|A|S|D|F|G|H|J|K|L|:|Z|X|C|V|B|N|M|<|>|\\?|\\||Й|Ц|У|К|Е|Н|Г|Ш|Щ|З|Х|Ъ|Ф|Ы|В|А|П|Р|О|Л|Д|Ж|Э|Я|Ч|С|М|И|Т|Ь|Б|Ю|й|ц|у|к|е|н|г|ш|щ|з|х|ъ|ф|ы|в|а|п|р|о|л|д|ж|э|я|ч|с|м|и|т|ь|б|ю|"|№|;| ';
 
 const CharsExp = new RegExp('(' + CharsExprStr + ')', 'g');
 const CharsExp2 = new RegExp('(' + CharsExprStr + '|\n)', 'g');
@@ -54,7 +54,7 @@ IMagick.GetTextSize = function(text, font, size) {
 		if (IMagick.PrecacheFontsData[font][p1]) {
 			widths[cLine] += IMagick.PrecacheFontsData[font][p1] * mult;
 		} else {
-			widths[cLine] += 12 * mult;
+			widths[cLine] += IMagick.PrecacheFontsData[font]['W'] * mult;
 		}
 		
 		return '';
@@ -125,7 +125,7 @@ let loadingStage3 = function() {
 			
 			if (data && data[0]) {
 				for (let row of data) {
-					IMagick.PrecacheFontsData[font][row.CHAR.trim()] = Number(row.WIDTH);
+					IMagick.PrecacheFontsData[font][row.CHAR.trim().replace('\\\\', '\\')] = Number(row.WIDTH);
 				}
 				
 				Postgres.query('SELECT * FROM font_height WHERE "ID" = ' + IMagick.FontIDs[font], function(_, data) {
@@ -179,7 +179,7 @@ let loadingStage3 = function() {
 							width = Math.floor(width / 2);
 						}
 						
-						finalQuery += 'INSERT INTO font_sizes ("ID", "CHAR", "WIDTH") VALUES (\'' + IMagick.FontIDs[font] + '\', ' + Util.escape(Char) + ', \'' + width + '\');';
+						finalQuery += 'INSERT INTO font_sizes ("ID", "CHAR", "WIDTH") VALUES (\'' + IMagick.FontIDs[font] + '\', ' + Util.escape(oldChar) + ', \'' + width + '\');';
 						
 						if (Char == 'W') {
 							finalQuery += 'INSERT INTO font_height ("ID", "HEIGHT") VALUES (\'' + IMagick.FontIDs[font] + '\', \'' + height + '\') ON CONFLICT DO NOTHING;';
@@ -415,6 +415,10 @@ IMagick.DrawText = function(data, callback) {
 						
 						if (chr == '\\') {
 							chr = '\\\\';
+						}
+						
+						if (chr == 'W') {
+							console.log(charWidth);
 						}
 						
 						if (gravity == 'center' || gravity == 'south' || gravity == 'north') {
