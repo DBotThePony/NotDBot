e41f566ee5c48f0b36ccb4003572e718e25af963
DBot <roboderpy@gmail.com>
2016-12-11 21:20:30 +0700

    nfs command

---
+++ app/commands/nfs.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+	name: 'nfs',
+	alias: ['needforsleep'],
+	
+	help_args: '',
+	desc: 'Need For Sleep',
+	allowUserArgument: true,
+	
+	func: function(args, cmd, msg) {
+		fs.readFile('./resource/files/nfs.png', {encoding: null}, function(err, data) {
+			if (!data)
+				return;
+			
+			if (msg.wasDeleted)
+				return;
+			
+			msg.channel.sendFile(data, 'yn.jpg').then(function(msg2) {
+				if (msg.wasDeleted)
+					msg2.delete(0);
+				else {
+					msg.replies = msg.replies || [];
+					msg.replies.push(msg2);
+				}
+			});
+		});
+	}
+}
