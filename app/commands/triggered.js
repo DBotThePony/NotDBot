aa39079c546abbecfb577d9d018e90f309f2678f
DBot <roboderpy@gmail.com>
2016-12-12 15:29:17 +0700

    Triggered2

--- app/commands/triggered.js
+++ app/commands/triggered.js
@@ -85,3 +85,74 @@ module.exports = {
 		});
 	},
 }
+
+DBot.RegisterCommand({
+	name: 'triggered2',
+	
+	argNeeded: true,
+	allowUserArgument: true,
+	
+	help_args: '<user>',
+	desc: '<TRIGGERED> variation 2',
+	
+	func: function(args, cmd, msg) {
+		let url = args[0];
+		
+		if (typeof url == 'object') {
+			url = args[0].avatarURL;
+			
+			if (!url)
+				return DBot.CommandError('User have no avatar? ;n;', 'triggered2', args, 1);
+		}
+		
+		if (!url)
+			return DBot.CommandError('Invalid url maybe? ;w;', 'triggered2', args, 1);
+		
+		let uObj = URL.parse(url);
+		let path = uObj.pathname;
+		let split = path.split('.');
+		let ext = split[split.length - 1].toLowerCase();
+		
+		if (!DBot.HaveValue(allowed, ext))
+			return DBot.CommandError('Invalid url maybe? ;w;', 'triggered2', args, 1);
+		
+		let sha = DBot.HashString(url);
+		
+		let fpath;
+		let fpathProcessed = DBot.WebRoot + '/triggered/' + sha + '_2.png';
+		let fpathU = DBot.URLRoot + '/triggered/' + sha + '_2.png';
+		
+		msg.channel.startTyping();
+		
+		let ContinueFunc = function() {
+			fs.stat(fpathProcessed, function(err, stat) {
+				if (stat) {
+					msg.channel.stopTyping();
+					msg.reply(fpathU);
+				} else {
+					let magik = spawn('convert', ['(', fpath, '-resize', '512', '-motion-blur', '0x15', ')', './resource/files/triggered.jpg', '-append', fpathProcessed]);
+					
+					Util.Redirect(magik);
+					
+					magik.on('close', function(code) {
+						msg.channel.stopTyping();
+						
+						if (code == 0) {
+							msg.reply(fpathU);
+						} else {
+							msg.reply('I am cracked up ;w;');
+						}
+					});
+				}
+			});
+		}
+		
+		DBot.LoadImageURL(url, function(newPath) {
+			fpath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	},
+});
