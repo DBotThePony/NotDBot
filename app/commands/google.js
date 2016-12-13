f36368a9f1e3e1868af23085ee8bd6fdc74cdbf0
DBot <roboderpy@gmail.com>
2016-12-13 18:00:55 +0700

    Optimized google commands

--- app/commands/google.js
+++ app/commands/google.js
@@ -31,19 +31,9 @@ DBot.CreateTagsSpace('google', [
 	'pornhub',
 ]);
 
-module.exports = {
-	name: 'google',
-	alias: ['search'],
-	
-	argNeeded: true,
-	delay: 3,
-	more: true,
-	
-	help_args: '<phrase>',
-	desc: 'Search a site in google',
-	
-	func: function(args, cmd, msg, previousStuff) {
-		var enc = encodeURIComponent(cmd.toLowerCase());
+let fn = function(prefix) {
+	return function(args, cmd, msg, previousStuff) {
+		var enc = encodeURIComponent(prefix + cmd.toLowerCase());
 		var url = Search + enc;
 		var hash = DBot.HashString(enc);
 		var cachePath = DBot.WebRoot + '/google/' + hash + '.json';
@@ -152,6 +142,20 @@ module.exports = {
 	}
 }
 
+module.exports = {
+	name: 'google',
+	alias: ['search'],
+	
+	argNeeded: true,
+	delay: 3,
+	more: true,
+	
+	help_args: '<phrase>',
+	desc: 'Search a site in google',
+	
+	func: fn(''),
+}
+
 DBot.RegisterCommand({
 	name: 'wiki',
 	alias: ['wikipedia'],
@@ -163,114 +167,7 @@ DBot.RegisterCommand({
 	help_args: '<phrase>',
 	desc: 'Search stuff in Wikipedia.\nThis uses Google tag ban space.',
 	
-	func: function(args, cmd, msg, previousStuff) {
-		var enc = encodeURIComponent('site:wikipedia.org ' + cmd.toLowerCase());
-		var url = Search + enc;
-		var hash = DBot.HashString(enc);
-		var cachePath = DBot.WebRoot + '/google/' + hash + '.json';
-		
-		var ServerTags;
-		var ClientTags = DBot.UserTags(msg.author, 'google');
-		var ChannelTags;
-		
-		if (!DBot.IsPM(msg)) {
-			ChannelTags = DBot.ChannelTags(msg.channel, 'google');
-			ServerTags = DBot.ServerTags(msg.channel.guild, 'google');
-		}
-		
-		if (!(msg.channel.name || 'private').match('nsfw')) {
-			for (var k in args) {
-				var split = args[k].split(' ');
-				
-				for (var i in split) {
-					if (ClientTags.isBanned(split[i]) || ServerTags && ServerTags.isBanned(split[i]) || ChannelTags && ChannelTags.isBanned(split[i])) {
-						return 'Search string contains tags that was banned by server, client or even you ;n;';
-					}
-				}
-			}
-		}
-		
-		msg.channel.startTyping();
-		
-		var continueSearch = function(data, isCached) {
-			msg.channel.stopTyping();
-			
-			try {
-				var items = data.items;
-				
-				if (!items || !items[0])
-					return msg.reply('None found ;n;');
-				
-				var items2;
-				
-				if (!previousStuff)
-					items2 = items;
-				else {
-					items2 = [];
-					
-					for (var i2 in items) {
-						var hit = false;
-						
-						for (var i in previousStuff) {
-							if (previousStuff[i] == items[i2].link) {
-								hit = true;
-								break;
-							}
-						}
-						
-						if (!hit)
-							items2.push(items[i2]);
-					}
-				}
-				
-				if (!items2[0]) {
-					msg.reply('Onoh! No more results ;n;');
-					return;
-				}
-				
-				var result;
-				
-				if (previousStuff)
-					result = DBot.RandomArray(items2);
-				else
-					result = items2[0];
-				
-				var output = 'Title: ' + result.title;
-				
-				if (isCached) {
-					output = '(results are cached)\n' + output;
-				}
-				
-				output += '\n```' + result.snippet + '```\n' + result.link;
-				
-				msg.reply(output);
-			} catch(err) {
-				msg.reply('<internal pony error>');
-				console.error(err);
-			}
-		}
-		
-		fs.stat(cachePath, function(err, stat) {
-			if (stat) {
-				fs.readFile(cachePath, 'utf8', function(err, data) {
-					continueSearch(JSON3.parse(data), true);
-				});
-			} else {
-				unirest.get(url)
-				.end(function(result) {
-					if (!result.body) {
-						msg.channel.stopTyping();
-						msg.reply('wtf with google');
-						return;
-					}
-					
-					continueSearch(result.body);
-					
-					fs.writeFile(cachePath, result.raw_body);
-				});
-			}
-		});
-	}
+	func: fn('site:wikipedia.org '),
 });
 
 DBot.RegisterCommand({
@@ -284,112 +181,5 @@ DBot.RegisterCommand({
 	help_args: '<phrase>',
 	desc: 'Search stuff in YouTube.\nThis uses Google tag ban space.',
 	
-	func: function(args, cmd, msg, previousStuff) {
-		var enc = encodeURIComponent('site:youtube.com ' + cmd.toLowerCase());
-		var url = Search + enc;
-		var hash = DBot.HashString(enc);
-		var cachePath = DBot.WebRoot + '/google/' + hash + '.json';
-		
-		var ServerTags;
-		var ClientTags = DBot.UserTags(msg.author, 'google');
-		var ChannelTags;
-		
-		if (!DBot.IsPM(msg)) {
-			ChannelTags = DBot.ChannelTags(msg.channel, 'google');
-			ServerTags = DBot.ServerTags(msg.channel.guild, 'google');
-		}
-		
-		if (!(msg.channel.name || 'private').match('nsfw')) {
-			for (var k in args) {
-				var split = args[k].split(' ');
-				
-				for (var i in split) {
-					if (ClientTags.isBanned(split[i]) || ServerTags && ServerTags.isBanned(split[i]) || ChannelTags && ChannelTags.isBanned(split[i])) {
-						return 'Search string contains tags that was banned by server, client or even you ;n;';
-					}
-				}
-			}
-		}
-		
-		msg.channel.startTyping();
-		
-		var continueSearch = function(data, isCached) {
-			msg.channel.stopTyping();
-			
-			try {
-				var items = data.items;
-				
-				if (!items || !items[0])
-					return msg.reply('None found ;n;');
-				
-				var items2;
-				
-				if (!previousStuff)
-					items2 = items;
-				else {
-					items2 = [];
-					
-					for (var i2 in items) {
-						var hit = false;
-						
-						for (var i in previousStuff) {
-							if (previousStuff[i] == items[i2].link) {
-								hit = true;
-								break;
-							}
-						}
-						
-						if (!hit)
-							items2.push(items[i2]);
-					}
-				}
-				
-				if (!items2[0]) {
-					msg.reply('Onoh! No more results ;n;');
-					return;
-				}
-				
-				var result;
-				
-				if (previousStuff)
-					result = DBot.RandomArray(items2);
-				else
-					result = items2[0];
-				
-				var output = 'Title: ' + result.title;
-				
-				if (isCached) {
-					output = '(results are cached)\n' + output;
-				}
-				
-				output += '\n```' + result.snippet + '```\n' + result.link;
-				
-				msg.reply(output);
-			} catch(err) {
-				msg.reply('<internal pony error>');
-				console.error(err);
-			}
-		}
-		
-		fs.stat(cachePath, function(err, stat) {
-			if (stat) {
-				fs.readFile(cachePath, 'utf8', function(err, data) {
-					continueSearch(JSON3.parse(data), true);
-				});
-			} else {
-				unirest.get(url)
-				.end(function(result) {
-					if (!result.body) {
-						msg.channel.stopTyping();
-						msg.reply('wtf with google');
-						return;
-					}
-					
-					continueSearch(result.body);
-					
-					fs.writeFile(cachePath, result.raw_body);
-				});
-			}
-		});
-	}
+	func: fn('site:youtube.com '),
 });
