debd07cc8514095d2eef7eb21429895be84f1a64
DBot <roboderpy@gmail.com>
2016-12-20 19:20:27 +0700

    Added cappend command

---
+++ app/commands/cappend.js
@@ -0,0 +1,101 @@
+
+const child_process = require('child_process');
+const spawn = child_process.spawn;
+const fs = DBot.fs;
+
+Util.mkdir(DBot.WebRoot + '/cappend');
+
+module.exports = {
+	name: 'cappend',
+	
+	help_args: '<url>',
+	desc: 'Broken seam-carving resize ;n;',
+	allowUserArgument: true,
+	delay: 5,
+	
+	func: function(args, cmd, msg) {
+		let url = args[0];
+		
+		if (typeof(url) == 'object') {
+			url = url.avatarURL;
+			
+			if (!url) {
+				return 'Specified user have no avatar? ;w;';
+			}
+		}
+		
+		url = url || DBot.LastURLImageInChannel(msg.channel);
+		if (!url)
+			return DBot.CommandError('Invalid url maybe? ;w;', 'cappend', args, 1);
+		
+		if (!DBot.CheckURLImage(url))
+			return DBot.CommandError('Invalid url maybe? ;w;', 'cappend', args, 1);
+		
+		let hash = DBot.HashString(url);
+		
+		let fPath;
+		let fPathProcessed = DBot.WebRoot + '/cappend/' + hash + '.png';
+		let fPathProcessedURL = DBot.URLRoot + '/cappend/' + hash + '.png';
+		
+		msg.channel.startTyping();
+		
+		let ContinueFunc = function() {
+			fs.stat(fPathProcessed, function(err, stat) {
+				if (stat) {
+					msg.channel.stopTyping();
+					msg.reply(fPathProcessedURL);
+				} else {
+					let magik = spawn('convert', [
+						fPath,
+						
+						'(',
+							fPath,
+							'-flop',
+						')',
+						
+						'+append',
+						
+						'(',
+							'(',
+								fPath,
+								'-flip',
+							')',
+							
+							'(',
+								fPath,
+								'-flop',
+								'-flip',
+							')',
+							
+							'+append',
+						')',
+						
+						'-append',
+						
+						fPathProcessed
+					]);
+					
+					Util.Redirect(magik);
+					
+					magik.on('close', function(code) {
+						if (code == 0) {
+							msg.channel.stopTyping();
+							msg.reply(fPathProcessedURL);
+						} else {
+							msg.channel.stopTyping();
+							msg.reply('Cracked up.');
+						}
+					});
+				}
+			});
+		}
+		
+		DBot.LoadImageURL(url, function(newPath) {
+			fPath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	}
+}
