beaa55dbf46e2a86105e50dd2c39c345710b3d2b
DBot <roboderpy@gmail.com>
2016-12-22 18:57:09 +0700

    Added flood detect by messages count and char count

--- app/modules/flood.js
+++ app/modules/flood.js
@@ -3,7 +3,12 @@ cvars.ChannelVar('noflood', '0', [FCVAR_BOOLONLY], 'Try to prevent channel flood
 cvars.ChannelVar('flood_newlines', '15', [FCVAR_NUMERSONLY_UINT], 'Limit of new lines in one messages');
 cvars.ChannelVar('flood_newlines_thersold', '75', [FCVAR_NUMERSONLY_UINT], 'Max of all new lines in all messages for last x seconds');
 cvars.ChannelVar('flood_newlines_cooldown', '5', [FCVAR_NUMERSONLY_UINT], 'Cooldown of thersold in seconds');
-cvars.ChannelVar('flood_trigger', '5', [FCVAR_NUMERSONLY_UINT], 'How much time user should wait after flood spam was triggered in seconds');
+cvars.ChannelVar('flood_trigger', '5', [FCVAR_NUMERSONLY_UINT], 'How much time user should wait after flood spam was triggered in seconds. This works on all triggers of flood detect');
+
+cvars.ChannelVar('flood_messages', '10', [FCVAR_NUMERSONLY_UINT], 'Thersold of messages in X seconds');
+cvars.ChannelVar('flood_chars', '3000', [FCVAR_NUMERSONLY_UINT], 'Thersold of chars sended in X seconds');
+cvars.ChannelVar('flood_messages_time', '4', [FCVAR_NUMERSONLY_UINT], 'Reset time of messages count');
+cvars.ChannelVar('flood_chars_time', '4', [FCVAR_NUMERSONLY_UINT], 'Reset time of chars count');
 
 hook.Add('PreOnValidMessage', 'AntiFlood', function(msg) {
 	if (DBot.IsPM(msg))
@@ -18,7 +23,8 @@ hook.Add('PreOnValidMessage', 'AntiFlood', function(msg) {
 	if (!cv.getVar('noflood').getBool())
 		return;
 	
-	let cnt = msg.content.split('\n').length
+	let cnt = msg.content.split('\n').length;
+	let chars = msg.content.length;
 	
 	if (member.flood_trigger && member.flood_trigger > CurTime()) {
 		if (!member.user.bot) {
@@ -35,12 +41,28 @@ hook.Add('PreOnValidMessage', 'AntiFlood', function(msg) {
 	}
 	
 	member.flood_newlines_thersold_last = member.flood_newlines_thersold_last || 0;
+	member.flood_messages_time = member.flood_messages_time || 0;
+	member.flood_chars_time = member.flood_chars_time || 0;
 	
 	if (member.flood_newlines_thersold_last < CurTime()) {
 		member.flood_newlines_thersold_last = CurTime() + cv.getVar('flood_newlines_cooldown').getInt();
 		member.flood_newlines_thersold = cnt;
 	} else {
 		member.flood_newlines_thersold += cnt;
+	}
+	
+	if (member.flood_messages_time < CurTime()) {
+		member.flood_messages_time = CurTime() + cv.getVar('flood_messages_time').getInt();
+		member.flood_messages = 1;
+	} else {
+		member.flood_messages++;
+	}
+	
+	if (member.flood_chars_time < CurTime()) {
+		member.flood_chars_time = CurTime() + cv.getVar('flood_chars_time').getInt();
+		member.flood_chars = chars;
+	} else {
+		member.flood_chars += chars;
 	}
 	
 	if (cnt > cv.getVar('flood_newlines').getInt()) {
@@ -63,6 +85,38 @@ hook.Add('PreOnValidMessage', 'AntiFlood', function(msg) {
 			
 			if (member.lastNotifyMessage < CurTime()) {
 				msg.author.sendMessage('You send too many messages with much new lines in so little time ;n;\nFlood ban is triggered ;w;');
+				member.lastNotifyMessage = CurTime() + 2;
+			}
+		}
+		
+		member.flood_trigger = CurTime() + cv.getVar('flood_trigger').getInt();
+		
+		msg.delete();
+		return true;
+	}
+	
+	if (member.flood_chars > cv.getVar('flood_chars').getInt()) {
+		if (!member.user.bot) {
+			member.lastNotifyMessage = member.lastNotifyMessage || 0;
+			
+			if (member.lastNotifyMessage < CurTime()) {
+				msg.author.sendMessage('Too many chars was sended in so little time, flood ban is triggered ;w;');
+				member.lastNotifyMessage = CurTime() + 2;
+			}
+		}
+		
+		member.flood_trigger = CurTime() + cv.getVar('flood_trigger').getInt();
+		
+		msg.delete();
+		return true;
+	}
+	
+	if (member.flood_messages > cv.getVar('flood_messages').getInt()) {
+		if (!member.user.bot) {
+			member.lastNotifyMessage = member.lastNotifyMessage || 0;
+			
+			if (member.lastNotifyMessage < CurTime()) {
+				msg.author.sendMessage('Too many messages was sended in so little time, flood ban is triggered ;w;');
 				member.lastNotifyMessage = CurTime() + 2;
 			}
 		}
