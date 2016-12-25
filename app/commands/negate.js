a17953db2c8c6a7de5bbe185895d386214168180
DBot <roboderpy@gmail.com>
2016-12-25 19:58:41 +0700

    Added negate command

---
+++ app/commands/negate.js
@@ -0,0 +1,69 @@
+
+const child_process = require('child_process');
+const spawn = child_process.spawn;
+const fs = DBot.fs;
+
+Util.mkdir(DBot.WebRoot + '/negate')
+
+module.exports = {
+	name: 'negate',
+	
+	help_args: '<image>',
+	desc: 'Negates image channels',
+	allowUserArgument: true,
+	
+	func: function(args, cmd, msg) {
+		let url = args[0];
+		
+		if (typeof(url) == 'object') {
+			url = url.avatarURL;
+			
+			if (!url) {
+				return 'Specified user have no avatar? ;w;';
+			}
+		}
+		
+		url = url || DBot.LastURLImageInChannel(msg.channel);
+		if (!DBot.CheckURLImage(url))
+			return DBot.CommandError('Invalid url maybe? ;w;', 'negate', args, 1);
+		
+		let hash = DBot.HashString(url);
+		
+		let fPath;
+		let fPathProcessed = DBot.WebRoot + '/negate/' + hash + '.png';
+		let fPathProcessedURL = DBot.URLRoot + '/negate/' + hash + '.png';
+		
+		msg.channel.startTyping();
+		
+		let ContinueFunc = function() {
+			fs.stat(fPathProcessed, function(err, stat) {
+				if (stat) {
+					msg.channel.stopTyping();
+					msg.reply(fPathProcessedURL);
+				} else {
+					let magik = spawn('convert', [fPath, '-negate', fPathProcessed]);
+					
+					Util.Redirect(magik);
+					
+					magik.on('close', function(code) {
+						if (code == 0) {
+							msg.reply(fPathProcessedURL);
+						} else {
+							msg.reply('*DED*');
+						}
+						
+						msg.channel.stopTyping();
+					});
+				}
+			});
+		}
+		
+		DBot.LoadImageURL(url, function(newPath, newExt) {
+			fPath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	}
+}
