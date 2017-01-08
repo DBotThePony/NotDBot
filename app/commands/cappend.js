20e646e1e4dcf3e5ee32fbf658ebd27a03e26142
DBot <roboderpy@gmail.com>
2017-01-08 08:04:48 +0700

    Added 2append command

--- app/commands/cappend.js
+++ app/commands/cappend.js
@@ -100,3 +100,79 @@ module.exports = {
 		});
 	}
 }
+
+DBot.RegisterCommand({
+	name: '2cappend',
+	alias: ['2layer'],
+	
+	help_args: '<url>',
+	desc: 'Appends image two times',
+	allowUserArgument: true,
+	delay: 5,
+	
+	func: function(args, cmd, msg) {
+		let url = args[0];
+		
+		if (typeof(url) == 'object') {
+			url = url.avatarURL;
+			
+			if (!url) {
+				return 'Specified user have no avatar? ;w;';
+			}
+		}
+		
+		url = url || DBot.LastURLImageInChannel(msg.channel);
+		if (!url)
+			return DBot.CommandError('Invalid url maybe? ;w;', 'cappend', args, 1);
+		
+		if (!DBot.CheckURLImage(url))
+			return DBot.CommandError('Invalid url maybe? ;w;', 'cappend', args, 1);
+		
+		let hash = DBot.HashString(url);
+		
+		let fPath;
+		let fPathProcessed = DBot.WebRoot + '/cappend/' + hash + '.png';
+		let fPathProcessedURL = DBot.URLRoot + '/cappend/' + hash + '.png';
+		
+		msg.channel.startTyping();
+		
+		let ContinueFunc = function() {
+			fs.stat(fPathProcessed, function(err, stat) {
+				if (stat) {
+					msg.channel.stopTyping();
+					msg.reply(fPathProcessedURL);
+				} else {
+					let magik = spawn('convert', [
+						fPath,
+						'(',
+							fPath,
+							'-flop',
+						')',
+						'+append',
+						fPathProcessed
+					]);
+					
+					Util.Redirect(magik);
+					
+					magik.on('close', function(code) {
+						if (code == 0) {
+							msg.channel.stopTyping();
+							msg.reply(fPathProcessedURL);
+						} else {
+							msg.channel.stopTyping();
+							msg.reply('Cracked up.');
+						}
+					});
+				}
+			});
+		}
+		
+		DBot.LoadImageURL(url, function(newPath) {
+			fPath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	}
+});
