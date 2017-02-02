6746dadfe29e3e68c48cdce499fc930bccc6bcf9
DBot <roboderpy@gmail.com>
2017-02-02 07:55:19 +0700

    Added eval command

---
+++ app/commands/eval.js
@@ -0,0 +1,30 @@
+
+module.exports = {
+	name: 'eval',
+	alias: ['debug'],
+	
+	help_args: '<code>',
+	desc: 'Bot owner only',
+	help_hide: true,
+	
+	func: function(args, cmd, msg) {
+		if (!DBot.owners.includes(msg.author.id))
+			return 'Not a bot owner';
+		
+		let myValue;
+		
+		try {
+			myValue = eval(cmd);
+		} catch(err) {
+			msg.sendMessage('```\n' + err.stack + '\n```');
+		}
+		
+		if (myValue === undefined) {
+			msg.sendMessage('```undefined```');
+		} else if (myValue === null) {
+			msg.sendMessage('```null```');
+		} else {
+			msg.sendMessage('```\n' + myValue.toString() + '\n```');
+		}
+	}
+};
