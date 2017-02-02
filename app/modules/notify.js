4780bd3848d141a6312b29e136a1b393fba25c6b
DBot <roboderpy@gmail.com>
2017-02-02 19:00:03 +0700

    Added notify_js, notify_join_e, notify_left_e

--- app/modules/notify.js
+++ app/modules/notify.js
@@ -1,7 +1,11 @@
 
-cvars.ServerVar('notifications', '0', [FCVAR_BOOLONLY], 'Enable join/leave notifications');
+cvars.ServerVar('notifications', '0', [FCVAR_BOOLONLY], 'Notifications main power switch');
+
+cvars.ServerVar('notify_jl', 'false', [FCVAR_BOOLONLY], 'Enable join/leave notifications');
 cvars.ServerVar('notify_join', 'Hello, %user%! Welcome to %server%! You are %num%rd member to join.', [], 'Join message. Use %username% for printing only user\'s name, not @mention him.');
+cvars.ServerVar('notify_join_e', 'true', [FCVAR_BOOLONLY], 'Enable join');
 cvars.ServerVar('notify_left', '%user% left us alone ;n;', [], 'Leave message');
+cvars.ServerVar('notify_left_e', 'true', [FCVAR_BOOLONLY], 'Enable leave');
 
 hook.Add('SoftbanJoinPass', 'Notifications', function(user, server, member) {
 	if (member.user.bot)
@@ -10,8 +14,9 @@ hook.Add('SoftbanJoinPass', 'Notifications', function(user, server, member) {
 	if (!DBot.ServerIsInitialized(server))
 		return;
 	
-	if (!cvars.Server(server).getVar('notifications').getBool())
-		return;
+	if (!cvars.Server(server).getVar('notifications').getBool()) return;
+	if (!cvars.Server(server).getVar('notify_jl').getBool()) return;
+	if (!cvars.Server(server).getVar('notify_join_e').getBool()) return;
 	
 	let channel = DBot.GetNotificationChannel(server);
 	
@@ -31,8 +36,9 @@ hook.Add('HumanLeftServer', 'Notifications', function(user, server, member) {
 	if (!DBot.ServerIsInitialized(server))
 		return;
 	
-	if (!cvars.Server(server).getVar('notifications').getBool())
-		return;
+	if (!cvars.Server(server).getVar('notifications').getBool()) return;
+	if (!cvars.Server(server).getVar('notify_jl').getBool()) return;
+	if (!cvars.Server(server).getVar('notify_left_e').getBool()) return;
 	
 	let channel = DBot.GetNotificationChannel(server);
 	
