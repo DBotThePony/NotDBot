bcaa845c0517f8f89dd48bd484d805a1448f2711
DBot <roboderpy@gmail.com>
2017-02-10 18:46:02 +0700

    Added about

---
+++ app/commands/about.js
@@ -0,0 +1,22 @@
+
+const text = '```\n\
+About:\n\
+NotDBot was created by DBot at 17 November 2016 as an experiment by DBot\n\
+Thanks for everyone who likes the bot, everyone who giving an ideas and tracking\n\
+bot issues. It makes me (DBot) feel good at this point, when i see that\n\
+my stuff is making everyone else happy. To invite bot use }invite, use }help for gettign help\n\
+You can change bot prefix using commands }svar and }cvar\n\
+Bot (main) issues tracker is located there: https://git.dbot.serealia.ca/dbot/NotDBot/issues\n\
+\n\
+Much thanks to all of you. By Konstantin "DBot" Linev.';
+
+module.exports = {
+	name: 'about',
+	
+	help_args: '',
+	desc: 'About',
+	
+	func: function(args, cmd, msg) {
+		return text;
+	}
+};
