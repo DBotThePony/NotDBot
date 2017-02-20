acf47999e99474e8e54bb6171c53aff4b5c2b431
DBot <roboderpy@gmail.com>
2017-02-20 13:03:39 +0700

    Reload command

---
+++ app/commands/reload.js
@@ -0,0 +1,45 @@
+
+const filesToReload = [
+	'../lib/sql_classes.js',
+	'../lib/sql_functions.js',
+	'../lib/sql_helpers.js',
+	'../commands.js'
+];
+
+module.exports = {
+	name: 'reload',
+	
+	help_args: '',
+	desc: 'Reloads some of code, reloads all commands',
+	
+	func: function(args, cmd, msg) {
+		if (!DBot.owners.includes(msg.author.id))
+			return 'Bot owner only';
+		
+		msg.channel.startTyping();
+		let cTime = CurTime();
+		
+		for (const file of filesToReload) {
+			try {
+				let recursiveFunc = function(data) {
+					if (data === undefined) return;
+					delete require.cache[data.id];
+					
+					for (const ch of data.children)
+						recursiveFunc(ch);
+				};
+				
+				recursiveFunc(require.cache[require.resolve(file)]);
+				
+				require(file);
+			} catch(err) {
+				msg.channel.stopTyping();
+				msg.sendMessage('```' + err.stack + '```');
+				return;
+			}
+		}
+		
+		msg.channel.stopTyping();
+		return 'Reload successfull. Done in ' + Math.floor((CurTime() - cTime) * 1000) + ' milliseconds.';
+	}
+};
