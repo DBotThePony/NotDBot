2eee6a7f323cbcb782c32d4963ecf27f6a807f88
DBot <roboderpy@gmail.com>
2017-02-22 10:14:32 +0700

    Who did this

---
+++ app/commands/whodidthis.js
@@ -0,0 +1,77 @@
+
+/* global DBot, Util, CommandHelper, MathHelper */
+
+const child_process = DBot.js.child_process;
+const spawn = child_process.spawn;
+const fs = DBot.js.filesystem;
+
+Util.mkdir(DBot.WebRoot + '/whodthis');
+
+module.exports = {
+	name: 'sepia',
+	
+	help_args: '<url>',
+	desc: 'Edits image with sepia effect',
+	allowUserArgument: true,
+	delay: 5,
+	
+	func: function(args, cmd, msg) {
+		const url = CommandHelper.CombinedURL(args[0], msg.channel);
+		
+		if (!url)
+			return DBot.CommandError('Invalid url maybe? ;w;', 'sepia', args, 1);
+		
+		let fPath;
+		const sha = String.hash(url);
+		const fPathProcessed = DBot.WebRoot + '/whodthis/' + sha + '.png';
+		const fPathProcessedURL = DBot.URLRoot + '/whodthis/' + sha + '.png';
+		
+		msg.channel.startTyping();
+		
+		const ContinueFunc = function() {
+			if (msg.checkAbort()) return;
+			
+			fs.stat(fPathProcessed, function(err, stat) {
+				if (msg.checkAbort()) return;
+				
+				if (stat) {
+					msg.channel.stopTyping();
+					msg.reply(fPathProcessedURL);
+				} else {
+					let magik = spawn('convert', [
+						'-size', '1024x760', 'xc:black', '-size', '740x640', 'xc:white', '-gravity', 
+						'center', '(', '-font', 'Roboto', '-pointsize', '72', '-draw', 
+						'text 0,-260 "Who did this??!!" image srcover -310,-270 96,96 "./resource/emoji/1f602.png" image srcover 310,-270 96,96 "./resource/emoji/1f602.png"', ')',
+						'-compose', 'srcover', '-composite',
+						'(', '-size', '600x550', 'xc:none', '-compose', 'srcover', '-gravity', 'south',
+						'(', fPath, '-resize', '460x460', ')', '-composite', ')',
+						'-gravity', 'center', '-composite',
+						fPathProcessed
+					]);
+					
+					Util.Redirect(magik);
+					
+					magik.on('close', function(code) {
+						msg.channel.stopTyping();
+						
+						if (!code) {
+							msg.reply(fPathProcessedURL);
+						} else {
+							msg.reply('*squeak*');
+						}
+					});
+				}
+			});
+		};
+		
+		DBot.LoadImageURL(url, function(newPath) {
+			fPath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	}
+};
+
+
