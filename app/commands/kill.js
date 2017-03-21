c817aad74fb9f10796402c73493bef95e19c52ec
DBot <roboderpy@gmail.com>
2017-03-21 21:06:40 +0700

    Moved killicons from database to json file

--- app/commands/kill.js
+++ app/commands/kill.js
@@ -10,49 +10,9 @@ const Postgres = myGlobals.Postgres;
 const CommandHelper = myGlobals.CommandHelper;
 
 const fs = require('fs');
+const json3 = require('json3');
 const child_process = require('child_process');
 const spawn = child_process.spawn;
-
-Postgres.query('SELECT COUNT(*) AS CNT FROM killicons', function(err, data) {
-	if (data && data[0] && data[0].CNT != 0)
-		return;
-	
-	fs.readdir('./resource/killicons', function(err, files) {
-		let names = ['NAME', 'FILENAME', 'CLASSNAME', 'WIDTH', 'HEIGHT'];
-		for (let file of files) {
-			let Class = file.substr(0, file.length - 4);
-			let name = Class.replace(/_/gi, ' ');
-			
-			let magik = spawn('identify', ['./resource/killicons/' + file]);
-			
-			let output = '';
-			
-			magik.stderr.on('data', function(data) {
-				console.error(data.toString());
-			});
-			
-			magik.stdout.on('data', function(data) {
-				output += data.toString();
-			});
-			
-			magik.on('close', function(code) {
-				let parse = output.split(' ');
-				
-				let fileName = parse[0];
-				let fileType = parse[1];
-				let fileSizes = parse[2];
-				
-				let fileSizesS = fileSizes.split('x');
-				let width = fileSizesS[0];
-				let height = fileSizesS[1];
-				let aspectRatio = height / width;
-				let aspectRatio2 = width / height;
-				
-				Postgres.query(sql.Insert('killicons', names, [name, file, Class, width, height]));
-			});
-		}
-	});
-});
 
 Util.mkdir(DBot.WebRoot + '/killfeed');
 
@@ -67,108 +27,112 @@ hook.Add('PrecacheFonts', 'KillIcon', function() {
 	IMagick.PrecacheFont(Font);
 });
 
-let generateFunc = function(col1, col2) {
-	return function(args, cmd, msg) {
+const weaponsData = json3.parse(fs.readFileSync('./resource/killicons_data.json'));
+
+const generateFunc = (col1, col2, cName) => {
+	return (args, cmd, msg) => {
 		if (!args[0])
 			return DBot.CommandError('No user', 'kill', args, 1);
 		
 		let username = args[0];
 		
-		if (typeof username == 'object')
+		if (typeof username === 'object')
 			username = username.username;
 		
 		if (!args[1])
 			return DBot.CommandError('No weapon name', 'kill', args, 1);
 		
-		let weapon = args[1].toLowerCase();
+		const weapon = args[1].toLowerCase();
 		
 		let username2 = args[2];
 		
-		if (typeof username2 == 'object')
+		if (typeof username2 === 'object')
 			username2 = username2.username;
 		
-		let sha = String.hash(username + ' ' + weapon + ' ' + username2);
+		let myWeapon = weaponsData[weapon];
+
+		if (!myWeapon) {
+			let min = 9999;
+
+			for (const wepClass in weaponsData) {
+				if (wepClass.match(weapon) && wepClass.length < min) {
+					min = wepClass.length;
+					myWeapon = weaponsData[wepClass];
+				}
+			}
+		}
+
+		if (!myWeapon)
+			return DBot.CommandError('No such weapon', cName, args, 2);
+
+		let sha = String.hash(username + ' ' + myWeapon.filename + ' ' + username2);
 		let fpath = DBot.WebRoot + '/killfeed/' + sha + '.png';
 		let fpathU = DBot.URLRoot + '/killfeed/' + sha + '.png';
 		
 		msg.channel.startTyping();
-		
-		fs.stat(fpath, function(err, stat) {
+
+		fs.stat(fpath, (err, stat) => {
 			if (msg.checkAbort()) return;
+			
 			if (stat) {
 				msg.channel.stopTyping();
 				msg.reply(fpathU);
 				return;
 			}
 			
-			Postgres.query('SELECT * FROM killicons WHERE "CLASSNAME" = ' + Postgres.escape(weapon) + ' OR "NAME" = ' + Postgres.escape(weapon) + ' OR "NAME" LIKE ' + Postgres.escape('%' + weapon + '%') + ' OR "CLASSNAME" LIKE ' + Postgres.escape('%' + weapon + '%'), function(err, data) {
+			let width = Number(myWeapon.width);
+			let iheight = Number(myWeapon.height);
+			let height = 70;
+			let calcWidthFirst = IMagick.GetTextSize(username, Font, FontSize)[0];
+			let calcWidthLast = 0;
+
+			if (username2)
+				calcWidthLast = IMagick.GetTextSize(username2, Font, FontSize)[0] + 25;
+
+			let totalWidth = calcWidthFirst + width + calcWidthLast + 30;
+
+			if (!username2)
+				totalWidth += 40;
+
+			let magikArgs = [
+				'-size', totalWidth + 'x' + height,
+				'canvas:' + bg,
+				'-font', Font,
+				'-pointsize', FontSize,
+				'-fill', col1,
+				'-weight', '700',
+				'-gravity', 'northwest'
+			];
+
+			if (username2) {
+				magikArgs.push('-draw', 'text 40,20 ' + Postgres.escape(username));
+				magikArgs.push('-draw', 'image over ' + (calcWidthFirst + 40) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + myWeapon.filename + '"');
+				magikArgs.push('-fill', col2, '-draw', 'text ' + (45 + calcWidthFirst + width) + ',20 ' + Postgres.escape(username2));
+			} else {
+				magikArgs.push('-draw', 'image over 30,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + myWeapon.filename + '"');
+				magikArgs.push('-draw', 'text ' + (60 + width) +',20 ' + Postgres.escape(username));
+			}
+
+			magikArgs.push(fpath);
+
+			let magik = spawn('convert', magikArgs);
+
+			Util.Redirect(magik);
+
+			magik.on('close', function(code) {
 				if (msg.checkAbort()) return;
-				if (err) {
-					msg.channel.stopTyping();
+				msg.channel.stopTyping();
+
+				if (code !== 0) {
 					msg.reply('<internal pony error>');
 					return;
 				}
-				
-				if (!data || !data[0]) {
-					msg.channel.stopTyping();
-					msg.reply('No such weapon');
-					return;
-				}
-				
-				let width = Number(data[0].WIDTH);
-				let iheight = Number(data[0].HEIGHT);
-				let height = 70;
-				let calcWidthFirst = IMagick.GetTextSize(username, Font, FontSize)[0];
-				let calcWidthLast = 0;
-				
-				if (username2)
-					calcWidthLast = IMagick.GetTextSize(username2, Font, FontSize)[0] + 25;
-				
-				let totalWidth = calcWidthFirst + width + calcWidthLast + 30;
-				
-				if (!username2)
-					totalWidth += 40;
-				
-				let magikArgs = [
-					'-size', totalWidth + 'x' + height,
-					'canvas:' + bg,
-					'-font', Font,
-					'-pointsize', FontSize,
-					'-fill', col1,
-					'-weight', '700',
-					'-gravity', 'northwest',
-				];
-				
-				if (username2) {
-					magikArgs.push('-draw', 'text 40,20 ' + Postgres.escape(username));
-					magikArgs.push('-draw', 'image over ' + (calcWidthFirst + 40) + ',' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-fill', col2, '-draw', 'text ' + (45 + calcWidthFirst + width) + ',20 ' + Postgres.escape(username2));
-				} else {
-					magikArgs.push('-draw', 'image over 30,' + (height / 2 - iheight / 2) + ' 0,0 "./resource/killicons/' + data[0].FILENAME + '"');
-					magikArgs.push('-draw', 'text ' + (60 + width) +',20 ' + Postgres.escape(username));
-				}
-				
-				magikArgs.push(fpath);
-				
-				let magik = spawn('convert', magikArgs);
-				
-				Util.Redirect(magik);
-				
-				magik.on('close', function(code) {
-					if (msg.checkAbort()) return;
-					msg.channel.stopTyping();
-					
-					if (code != 0) {
-						msg.reply('<internal pony error>');
-						return;
-					}
-					
-					msg.reply(fpathU);
-				});
+
+				msg.reply(fpathU);
 			});
 		});
 	};
-}
+};
 
 module.exports = {
 	name: 'kill',
@@ -178,7 +142,7 @@ module.exports = {
 	desc: 'Generates TF2 kill text',
 	allowUserArgument: true,
 	
-	func: generateFunc(red, blu),
+	func: generateFunc(red, blu, 'kill')
 }
 
 DBot.RegisterCommand({
@@ -189,7 +153,7 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. Instead of RED / BLU uses BLU / RED',
 	allowUserArgument: true,
 	
-	func: generateFunc(blu, red),
+	func: generateFunc(blu, red, 'rkill')
 });
 
 DBot.RegisterCommand({
@@ -200,7 +164,7 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. TEAM KILLER!',
 	allowUserArgument: true,
 	
-	func: generateFunc(red, red),
+	func: generateFunc(red, red, 'tkill')
 });
 
 DBot.RegisterCommand({
@@ -211,5 +175,5 @@ DBot.RegisterCommand({
 	desc: 'Generates TF2 kill text. BLU TEAM KILLER!',
 	allowUserArgument: true,
 	
-	func: generateFunc(blu, blu),
+	func: generateFunc(blu, blu, 'rtkill')
 });
---
+++ tools/killicons.js
@@ -0,0 +1,59 @@
+
+process.env['PATH'] = '../bin;' + process.env['PATH'];
+
+const json3 = require('json3');
+const fs = require('fs');
+const child_process = require('child_process');
+const spawn = child_process.spawn;
+const basePath = '../resource/killicons/';
+const outputFile = '../resource/killicons_data.json';
+
+console.log('Reading directory');
+
+const files = fs.readdirSync(basePath);
+const args = [];
+
+for (const file of files) {
+	args.push(basePath + file);
+}
+
+let output = '';
+const identify = spawn('identify', args);
+
+identify.stderr.pipe(process.stderr);
+identify.stdout.on('data', (bin) => {
+	output += bin.toString();
+});
+
+console.log('Identifying');
+
+identify.on('close', (code) => {
+	if (code !== 0) {
+		console.error('identify exit with non zero code');
+		process.exit(code);
+	}
+
+	const split = output.replace(/\r/g, '').split('\n');
+	const outputData = {};
+
+	for (const outputString of split) {
+		const infos = outputString.split(' ');
+		if (!infos[1]) continue;
+		const sizes = infos[2].split('x');
+
+		const name = infos[0].substr(basePath.length);
+		const wepClass = name.substr(0, name.length - 4);
+		const width = Number(sizes[0]);
+		const height = Number(sizes[1]);
+
+		console.log(wepClass, ' => ', width + 'x' + height, 'pixels');
+
+		outputData[wepClass] = {
+			filename: name,
+			width: width,
+			height: height
+		};
+	}
+
+	fs.writeFileSync(outputFile, json3.stringify(outputData));
+});
