a2c82f3864ed31d099e9d85f6cc2d3c2632c2a31
DBot <roboderpy@gmail.com>
2017-04-07 16:42:38 +0700

    Added more emoji reactions

--- app/modules/autoreact.js
+++ app/modules/autoreact.js
@@ -25,40 +25,335 @@ const cvars = myGlobals.cvars;
 const emojiModule = require('../lib/emoji.js');
 
 const reactions = [
-	['lit', 'flame'],
-	['fire', 'flame'],
-	['hot', 'flame'],
-	['snek', 'snake'],
-	['ok', 'ok'],
-	['cool', 'sunglasses'],
-	['eye', 'eyes'],
-	['new', 'new'],
-	['free', 'free'],
-	['time', 'clock3'],
-	['scream', 'loudspeaker'],
-	['book', 'closed_book'],
-	['mail', 'mailbox_with_mail'],
-	['syringe', 'syringe'],
-	['bleed', 'syringe'],
-	['draco', 'dragon'],
-	['dragon', 'dragon'],
-	['cactus', 'cactus'],
-	['dog', 'poodle'],
-	['cat', 'cat2'],
-	['horse', 'racehorse'],
-	['mouse', 'mouse2'],
-	['butterfly', 'butterfly'],
-	['pizza', 'pizza'],
-	['tada', 'tada'],
-	['confetti', 'tada'],
-	['surprise', 'tada'],
-	['police', 'oncoming_police_car'],
-	['love', 'blue_heart']
+	[
+		['lit', 'fire', 'flame'],
+		['flame']
+	],
+	
+	[
+		['hot', 'lewd', 'lewds', 'lewdy'],
+		['thermometer']
+	],
+	
+	[
+		['snek', 'snaek', 'snake', 'danger noodle', 'sneaky'],
+		['snake']
+	],
+	
+	[
+		['ok', 'okay', 'okai'],
+		['ok']
+	],
+	
+	[
+		['cool', 'kool', 'sun glasses', 'sunglasses'],
+		['sunglasses']
+	],
+	
+	[
+		['eye', 'eyes'],
+		['eyes']
+	],
+	
+	[
+		['horse', 'pony', 'pone', 'ponie', 'pne'],
+		['racehorse']
+	],
+	
+	[
+		['tada', 'voila', 'confetti', 'surprise'],
+		['tada']
+	],
+	
+	[
+		['love', 'heart', 'loveheart', 'luv', 'luvs'],
+		['blue_heart', 'heart', 'hearts', 'yellow_heart', 'purple_heart', 'green_heart', 'two_hearts', 'heartbeat', 'cupid', 'revolving_hearts']
+	],
+	
+	[
+		['i miss', 'late'],
+		['alarm_clock']
+	],
+	
+	[
+		['loud', 'scream'],
+		['loudspeaker']
+	],
+	
+	[
+		['zap', 'lightning', 'fast', 'faster'],
+		['zap']
+	],
+	
+	[
+		['book', 'dictionary', 'read'],
+		['green_book', 'closed_book', 'blue_book', 'orange_book', 'books', 'book']
+	],
+	
+	[
+		['apple', 'apples'],
+		['green_apple', 'apple']
+	],
+	
+	[
+		['lemon', 'lemons'],
+		['lemon']
+	],
+	
+	[
+		['melon', 'melons'],
+		['watermelon']
+	],
+	
+	[
+		['strawberry', 'berry', 'berries'],
+		['strawberry']
+	],
+	
+	[
+		['peach', 'peaches'],
+		['peach']
+	],
+	
+	[
+		['bread'],
+		['bread']
+	],
+	
+	[
+		['candy'],
+		['candy']
+	],
+	
+	[
+		['lollipop'],
+		['lollipop']
+	],
+	
+	[
+		['chocolate'],
+		['chocolate_bar']
+	],
+	
+	[
+		['popcorn'],
+		['popcorn']
+	],
+	
+	[
+		['donut'],
+		['doughnut']
+	],
+	
+	[
+		['art', 'artistic'],
+		['art']
+	],
+	
+	[
+		['gift'],
+		['gift']
+	],
+	
+	[
+		['brilliant', 'gem', 'diamond', 'ruby', 'perfect'],
+		['gem']
+	],
+	
+	[
+		['hammer', 'destroy'],
+		['hammer']
+	],
+	
+	[
+		['fix', 'repair'],
+		['wrench']
+	],
+	
+	[
+		['bomb'],
+		['bomb']
+	],
+	
+	[
+		['scissors'],
+		['scissors']
+	],
+	
+	[
+		['gun', 'revolver', 'pistol', 'assault rifle'],
+		['gun']
+	],
+	
+	[
+		['fruit', 'fruits'],
+		['green_apple', 'apple', 'pear', 'tangerine', 'lemon', 'peach']
+	],
+	
+	[
+		['police'],
+		['oncoming_police_car']
+	],
+	
+	[
+		['new'],
+		['new']
+	],
+	
+	[
+		['!'],
+		['exclamation']
+	],
+	
+	[
+		['!!'],
+		['bangbang']
+	],
+	
+	[
+		['\\?'],
+		['question']
+	],
+	
+	[
+		['\\?!', '!\\?'],
+		['interrobang']
+	],
+	
+	[
+		['warning', 'onoh', 'oops', 'oh no', 'nooo+'],
+		['warning']
+	],
+	
+	[
+		['octagon', 'stop'],
+		['octagonal_sign']
+	],
+	
+	[
+		['lol', 'loal', 'joy', 'lmao', 'rofl'],
+		['joy', 'joy_cat']
+	],
+	
+	[
+		['free'],
+		['free']
+	],
+	
+	[
+		['mushroom'],
+		['mushroom']
+	],
+	
+	[
+		['spider', 'coweb'],
+		['spider_web']
+	],
+	
+	[
+		['mail'],
+		['mailbox_with_mail']
+	],
+	
+	[
+		['sunny', 'sun'],
+		['sun_with_face']
+	],
+	
+	[
+		['syringe'],
+		['syringe']
+	],
+	
+	[
+		['boom', 'explosion'],
+		['boom']
+	],
+	
+	[
+		['snowflake', 'snow', 'snowy', 'freezing', 'freezy', 'freeze'],
+		['snowflake']
+	],
+	
+	[
+		['bleed'],
+		['syringe']
+	],
+	
+	[
+		['lizard'],
+		['lizard']
+	],
+	
+	[
+		['draco'],
+		['dragon']
+	],
+	
+	[
+		['dragon'],
+		['dragon']
+	],
+	
+	[
+		['cactus'],
+		['cactus']
+	],
+	
+	[
+		['dog'],
+		['poodle']
+	],
+	
+	[
+		['cat'],
+		['cat2']
+	],
+	
+	[
+		['mouse'],
+		['mouse2']
+	],
+	
+	[
+		['butterfly', 'fly', 'light', 'soft'],
+		['butterfly']
+	],
+	
+	[
+		['shark'],
+		['shark']
+	],
+	
+	[
+		['pizza'],
+		['pizza']
+	]
 ];
 
+{
+	const clock = [];
+	
+	for (let i = 1; i <= 12; i++)
+		clock.push('clock' + i);
+	
+	reactions.push([['time', 'clock', 'oclock', 'alarm'], clock]);
+}
+
 for (const ar of reactions) {
-	ar[0] = new RegExp('(^| )' + ar[0] + '( |$)', 'i');
-	ar[2] = emojiModule.getEmojiByName(ar[1]);
+	ar[2] = [];
+}
+
+for (const ar of reactions) {
+	for (const trigger in ar[0]) {
+		ar[0][trigger] = new RegExp('(^| )' + ar[0][trigger] + '( |$|!)', 'i');
+	}
+	
+	for (const emoji in ar[1]) {
+		ar[2][emoji] = emojiModule.getEmojiByName(ar[1][emoji]);
+		if (!ar[2][emoji])
+			console.error(`Unable to find a map for ${ar[1][emoji]}!`);
+	}
 }
 
 cvars.ServerVar('autoreact', '1', [FCVAR_BOOLONLY], 'Lit reactions');
@@ -80,9 +375,12 @@ const OnValidMessage = function(msg) {
 	let i = 0;
 	
 	for (const data of reactions) {
-		if (msg.content.match(data[0])) {
-			i++;
-			msg.react(data[2]).catch(console.error);
+		for (const matchings of data[0]) {
+			if (msg.content.match(matchings)) {
+				i++;
+				msg.react(Array.Random(data[2])).catch(console.error);
+				break;
+			}
 		}
 		
 		if (i > 5)
