ea76d940ffec00dd2c0d68a225e9c768828dc736
DBot <roboderpy@gmail.com>
2017-04-08 16:40:11 +0700

    Added moar reactions

--- app/modules/autoreact.js
+++ app/modules/autoreact.js
@@ -24,9 +24,11 @@ const cvars = myGlobals.cvars;
 
 const emojiModule = require('../lib/emoji.js');
 
+
+
 const reactions = [
 	[
-		['lit', 'fire', 'flame'],
+		['lit', 'fire', 'flame', emojiModule.getEmojiByName('fire')],
 		['flame']
 	],
 	
@@ -36,12 +38,12 @@ const reactions = [
 	],
 	
 	[
-		['snek', 'snaek', 'snake', 'danger noodle', 'sneaky', 'hiss+'],
+		['snek', 'snaek', 'snake', 'danger noodle', 'sneaky', 'hiss+', emojiModule.getEmojiByName('snake')],
 		['snake']
 	],
 	
 	[
-		['ok', 'okay', 'okai'],
+		['ok', 'okay', 'okai', emojiModule.getEmojiByName('ok')],
 		['ok']
 	],
 	
@@ -56,6 +58,31 @@ const reactions = [
 	],
 	
 	[
+		['thinking', 'thokang', 'thokaning', emojiModule.getEmojiByName('thinking')],
+		['thinking']
+	],
+	
+	[
+		[':\\(', emojiModule.getEmojiByName('slight_frown')],
+		['slight_frown']
+	],
+	
+	[
+		[':\\)', emojiModule.getEmojiByName('grinning'), emojiModule.getEmojiByName('smiley')],
+		['grinning', 'smile_cat']
+	],
+	
+	[
+		[':P', emojiModule.getEmojiByName('stuck_out_tongue'), emojiModule.getEmojiByName('stuck_out_tongue_closed_eyes')],
+		['stuck_out_tongue', 'stuck_out_tongue_closed_eyes']
+	],
+	
+	[
+		[':D', emojiModule.getEmojiByName('smile')],
+		['smile']
+	],
+	
+	[
 		['horse', 'pony', 'pone', 'ponie', 'pne'],
 		['racehorse']
 	],
@@ -63,6 +90,21 @@ const reactions = [
 	[
 		['tada', 'voila', 'confetti', 'surprise'],
 		['tada']
+	],
+	
+	[
+		['magic', 'sparkles', emojiModule.getEmojiByName('sparkles')],
+		['sparkles']
+	],
+	
+	[
+		['o', emojiModule.getEmojiByName('open_mouth')],
+		['open_mouth']
+	],
+	
+	[
+		[':\\|', ':/', ':\\\\', emojiModule.getEmojiByName('confused')],
+		['confused']
 	],
 	
 	[
@@ -231,7 +273,7 @@ const reactions = [
 	],
 	
 	[
-		['lol', 'loal', 'joy', 'lmao', 'rofl'],
+		['lol', 'loal', 'joy', 'lmao', 'rofl', 'joy', 'xd', emojiModule.getEmojiByName('joy')],
 		['joy', 'joy_cat']
 	],
 	
@@ -333,7 +375,212 @@ const reactions = [
 	[
 		['pizza'],
 		['pizza']
+	],
+	
+	[
+		['twisted'],
+		['twisted_rightwards_arrows']
+	],
+	
+	[
+		['fast forward', 'forward'],
+		['fast_forward']
+	],
+	
+	[
+		['rewind'],
+		['rewind']
+	],
+	
+	// Numbers
+	
+	[
+		['one', '1'],
+		['one']
+	],
+
+	[
+		['two', '2'],
+		['two']
+	],
+
+	[
+		['three', '3'],
+		['three']
+	],
+
+	[
+		['four', '4'],
+		['four']
+	],
+
+	[
+		['five', '5'],
+		['five']
+	],
+
+	[
+		['six', '6'],
+		['six']
+	],
+
+	[
+		['seven', '7'],
+		['seven']
+	],
+
+	[
+		['eight', '8'],
+		['eight']
+	],
+
+	[
+		['nine', '9'],
+		['nine']
+	],
+
+	[
+		['zero', '0'],
+		['zero']
+	],
+
+	[
+		['keycap_ten', '10', 'ten'],
+		['keycap_ten']
 	]
+];
+
+const arrowsEmoji = [
+	'arrow_backward',
+	'arrow_down',
+	'arrow_forward',
+	'arrow_left',
+	'arrow_lower_left',
+	'arrow_lower_right',
+	'arrow_right',
+	'arrow_up',
+
+	'arrow_double_down',
+	'arrow_double_up',
+
+	'arrow_heading_down',
+	'arrow_heading_up',
+	'arrow_up_small'
+];
+
+const simpleEmojiReact = [
+	'sunny',
+	'umbrella',
+	'cloud',
+	'snowflake',
+	'snowman',
+	'zap',
+	'cyclone',
+	'foggy',
+	'ocean',
+	'cat',
+	'dog',
+	'mouse',
+	'hamster',
+	'rabbit',
+	'wolf',
+	'frog',
+	'tiger',
+	'koala',
+	'bear',
+	'pig',
+	'pig_nose',
+	'cow',
+	'boar',
+	'monkey_face',
+	'monkey',
+	'horse',
+	'racehorse',
+	'camel',
+	'sheep',
+	'elephant',
+	'panda_face',
+	'snake',
+	'bird',
+	'baby_chick',
+	'hatched_chick',
+	'hatching_chick',
+	'chicken',
+	'penguin',
+	'turtle',
+	'bug',
+	'ant',
+	'beetle',
+	'snail',
+	'octopus',
+	'tropical_fish',
+	'fish',
+	'whale',
+	'whale2',
+	'dolphin',
+	'cow2',
+	'ram',
+	'rat',
+	'water_buffalo',
+	'tiger2',
+	'rabbit2',
+	'dragon',
+	'goat',
+	'rooster',
+	'dog2',
+	'pig2',
+	'mouse2',
+	'ox',
+	'dragon_face',
+	'blowfish',
+	'crocodile',
+	'dromedary_camel',
+	'leopard',
+	'cat2',
+	'poodle',
+	'paw_prints',
+	'bouquet',
+	'cherry_blossom',
+	'tulip',
+	'four_leaf_clover',
+	'rose',
+	'sunflower',
+	'hibiscus',
+	'maple_leaf',
+	'leaves',
+	'fallen_leaf',
+	'herb',
+	'mushroom',
+	'cactus',
+	'palm_tree',
+	'evergreen_tree',
+	'deciduous_tree',
+	'chestnut',
+	'seedling',
+	'blossom',
+	'ear_of_rice',
+	'shell',
+	'globe_with_meridians',
+	'sun_with_face',
+	'full_moon_with_face',
+	'new_moon_with_face',
+	'new_moon',
+	'waxing_crescent_moon',
+	'first_quarter_moon',
+	'waxing_gibbous_moon',
+	'full_moon',
+	'waning_gibbous_moon',
+	'last_quarter_moon',
+	'waning_crescent_moon',
+	'last_quarter_moon_with_face',
+	'first_quarter_moon_with_face',
+	'crescent_moon',
+	'earth_africa',
+	'earth_americas',
+	'earth_asia',
+	'volcano',
+	'milky_way',
+	'partly_sunny'
 ];
 
 {
@@ -345,13 +592,29 @@ const reactions = [
 	reactions.push([['time', 'clock', 'oclock', 'alarm'], clock]);
 }
 
+for (const em of simpleEmojiReact) {
+	reactions.push([[emojiModule.getEmojiByName(em), em.replace(/_/g, ' '), em], [em]]);
+}
+
+for (const em of arrowsEmoji) {
+	reactions.push([
+		[emojiModule.getEmojiByName(em), em.replace(/_/g, ' '), em, em.substr(6), em.substr(6).replace(/_/g, '')],
+		[em]
+	]);
+}
+
 for (const ar of reactions) {
 	ar[2] = [];
 }
 
 for (const ar of reactions) {
 	for (const trigger in ar[0]) {
-		ar[0][trigger] = new RegExp('(^| )' + ar[0][trigger] + '( |$|!)', 'i');
+		try {
+			ar[0][trigger] = new RegExp('(^| )' + ar[0][trigger] + '( |$|!|\\*|\\.)', 'i');
+		} catch(err) {
+			console.error('Emoji: ' + ar[0] + ', outputs: ' + ar[1].join(', '));
+			console.error(err);
+		}
 	}
 	
 	for (const emoji in ar[1]) {
