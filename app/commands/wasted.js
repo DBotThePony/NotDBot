59dbcdc4a5c72828c3d1fda34339a906ac2a7827
DBot <roboderpy@gmail.com>
2017-05-04 16:42:17 +0700

    Cactus command

--- app/commands/wasted.js
+++ app/commands/wasted.js
@@ -168,7 +168,135 @@ module.exports = {
 			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
 		});
 	}
-}
+};
+
+DBot.RegisterCommand({
+	name: 'cactus',
+	
+	help_args: '<user>',
+	desc: 'you got cocky, mate',
+	allowUserArgument: true,
+	
+	func: function(args, cmd, msg) {
+		let url = CommandHelper.CombinedURL(args[0], msg.channel);
+		
+		if (!url)
+			return DBot.CommandError('Invalid url maybe? ;w;', 'cactus', args, 1);
+		
+		const hash = String.hash(url + '_cactus');
+		
+		let fPath;
+		
+		const fPathProcessed = DBot.WebRoot + '/wasted/' + hash + '.png';
+		const fPathProcessedURL = DBot.URLRoot + '/wasted/' + hash + '.png';
+		
+		msg.channel.startTyping();
+		
+		let ContinueFunc = function() {
+			if (msg.checkAbort()) return;
+			fs.stat(fPathProcessed, function(err, stat) {
+				if (msg.checkAbort()) return;
+				if (stat && stat.isFile()) {
+					msg.channel.stopTyping();
+					msg.reply(fPathProcessedURL);
+				} else {
+					IMagick.Identify(fPath, function(err, fileType, width, height, aspectRatio, aspectRatio2) {
+						if (msg.checkAbort()) return;
+						if (err) {
+							msg.channel.stopTyping();
+							msg.reply('<internal pony error>');
+							return;
+						}
+
+						let magikArgs = [fPath];
+
+						if (width < 512) {
+							magikArgs.push('-resize');
+
+							width = 512;
+							height = Math.floor(aspectRatio * 512);
+
+							magikArgs.push('512x' + height);
+						}
+
+						if (height < 512) {
+							magikArgs.push('-resize');
+
+							height = 512;
+							width = Math.floor(aspectRatio2 * 512);
+
+							magikArgs.push(width);
+						}
+
+						if (width > 1500 || height > 1500)
+							msg.reply('Big Picture OwO, Cropping to 1500x1500');
+
+						if (width > 1500) {
+							magikArgs.push('-resize');
+
+							width = 1500;
+							height = Math.floor(aspectRatio * 1500);
+
+							magikArgs.push('1500x' + height);
+						}
+
+						if (height > 1500) {
+							magikArgs.push('-resize');
+
+							height = 1500;
+							width = Math.floor(aspectRatio2 * 1500);
+
+							magikArgs.push(width);
+						}
+
+						magikArgs.push('-color-matrix', '.3 .1 .3 .3 .1 .3 .3 .1 .3', '-fill', 'rgba(0,0,0,0.5)');
+
+						let signHeight = height * .2;
+
+						magikArgs.push('-draw', 'rectangle 0, ' + (height / 2 - signHeight / 2) + ', ' + width + ', ' + (height / 2 + signHeight / 2));
+
+						magikArgs.push(
+							'-gravity', 'South',
+							'-font', 'PricedownBl-Regular',
+							'-fill', 'rgb(200,30,30)',
+							'-stroke', 'black',
+							'-strokewidth', '3',
+							'-weight', '300'
+						);
+
+						magikArgs.push('-pointsize', String(Math.floor(signHeight * .8)), '-draw', 'text 0,' + (Math.floor(height / 2 - (signHeight - 60) * .45)) + ' "cactus"');
+						magikArgs.push('-fill', 'rgb(237,226,226)', '-strokewidth', '2');
+						magikArgs.push('-pointsize', String(Math.floor(signHeight * .2)), '-draw', 'text 0,' + (Math.floor(height / 2 - (signHeight) * .45)) + ' "you got cocky, mate"');
+
+						magikArgs.push(fPathProcessed);
+
+						let magik = spawn('convert', magikArgs);
+
+						Util.Redirect(magik);
+
+						magik.on('close', function(code) {
+							msg.channel.stopTyping();
+
+							if (code == 0) {
+								msg.reply(fPathProcessedURL);
+							} else {
+								msg.reply('<internal pony error>');
+							}
+						});
+					});
+				}
+			});
+		}
+		
+		CommandHelper.loadImage(url, function(newPath) {
+			fPath = newPath;
+			ContinueFunc();
+		}, function(result) {
+			msg.channel.stopTyping();
+			msg.reply('Failed to download image. "HTTP Status Code: ' + (result.code || 'socket hangs up or connection timeout') + '"');
+		});
+	}
+});
 
 DBot.RegisterCommand({
 	name: 'dead',
